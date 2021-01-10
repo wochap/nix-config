2b6cf6ac13303ac2c44aeef81747dbf9143d1692
Gean <gean.marroquin@gmail.com>
2021-01-10 08:34:58 -0500

    refactor

--- nixos/users/common.nix
+++ nixos/users/common.nix
@@ -30,33 +30,6 @@
     # '';
   };
 
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "gean";
-  home.homeDirectory = "/home/gean";
-
-  # User packages
-  home.packages = with pkgs; [
-    # Basic tools
-    htop
-    wget
-    radeontop # like htop for gpu
-
-    # DE
-    nitrogen # wallpaper manager
-    copyq # clipboard manager
-    xfce.thunar # file manager
-
-    # Dev tools
-    gitAndTools.gh
-
-    # Apps
-    firefox
-    pantheon.elementary-icon-theme
-    vscode
-    nix-prefetch-git
-  ];
-
   home.sessionVariables = {
     # EDITOR = "nvim";
   };
--- nixos/users/polybar.nix
+++ nixos/users/polybar.nix
@@ -5,7 +5,7 @@
   services.polybar = {
     enable = true;
     script = "polybar mainbar-bspwm &";
-    # config = ../dotfiles/polybar-example.ini;
-    config = ../dotfiles/polybar.ini;
+    config = ../dotfiles/polybar-example.ini;
+    # config = ../dotfiles/polybar.ini;
   };
 }
