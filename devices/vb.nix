1fb078d386252e964b8f38c0fa9f05d5a591310c
Gean <gean.marroquin@gmail.com>
2021-01-30 16:46:37 -0500

    t

--- devices/vb.nix
+++ devices/vb.nix
@@ -20,12 +20,13 @@ in
 
     networking = {
       hostName = hostName;
+
+      # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+      # Per-interface useDHCP will be mandatory in the future, so this generated config
+      # replicates the default behaviour.
+      useDHCP = false;
+      interfaces.enp0s3.useDHCP = true;
     };
-    # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-    # Per-interface useDHCP will be mandatory in the future, so this generated config
-    # replicates the default behaviour.
-    networking.useDHCP = false;
-    networking.interfaces.enp0s3.useDHCP = true;
 
     sound.enable = true;
 
@@ -36,7 +37,7 @@ in
 
     services.xserver.videoDrivers = [
       "nv"
-      "nvidia"  
+      "nvidia"
       "nouveau"
     ];
   };
