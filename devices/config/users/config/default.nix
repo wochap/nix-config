234190e9dcfb103b4357e636ce4f04eb7c681294
Gean <gean.marroquin@gmail.com>
2021-01-30 17:10:29 -0500

    polybar

--- devices/config/users/config/default.nix
+++ devices/config/users/config/default.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./git.nix
+    ./polybar.nix
   ];
 
   config = {
---
+++ devices/config/users/config/polybar.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+{
+  config = {
+    home-manager.users.gean = {
+      services.polybar = {
+        enable = true;
+        script = "polybar mainbar-bspwm &";
+        config = ../dotfiles/polybar/example.ini;
+      };
+    };
+  };
+}
