93c5d0bfc12e971c103290bed99b1a9555cdb62b
wochap <gean.marroquin@gmail.com>
2021-02-03 22:52:07 -0500

    f

--- devices/config/users/config/polybar.nix
+++ devices/config/users/config/polybar.nix
@@ -2,20 +2,19 @@
 
 let
   isXorg = config._displayServer == "xorg";
+  isDesktop = config.networking.hostName == "gdesktop";
 in
 {
   config = {
     home-manager.users.gean = {
       services.polybar = lib.mkIf isXorg {
         enable = true;
-        script = ''
-          if type "xrandr"; then
-            for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-              MONITOR=$m polybar --reload mainbar-bspwm &
-            done
-          else
-            polybar --reload mainbar-bspwm &
-          fi
+        # TODO: try https://github.com/polybar/polybar/issues/763
+        script = if isDesktop then ''
+          MONITOR=DP-0 BAR_HEIGHT=52 polybar --reload mainbar-bspwm &
+          MONITOR=DP-2 BAR_HEIGHT=36 polybar --reload mainbar-bspwm &
+        '' else ''
+          polybar --reload mainbar-bspwm &
         '';
         config = ./dotfiles/polybar/config.ini;
       };
