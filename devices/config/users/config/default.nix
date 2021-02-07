4cd27b47025aab3070b1bf57d4a291d5b9cb40f2
wochap <gean.marroquin@gmail.com>
2021-02-07 16:58:15 -0500

    refactor fish config

--- devices/config/users/config/default.nix
+++ devices/config/users/config/default.nix
@@ -117,56 +117,7 @@ in
             end
           '';
 
-          "fish/config.fish".text = lib.mkAfter ''
-            set fish_greeting
-
-            set -U fish_color_autosuggestion      brblack
-            set -U fish_color_cancel              -r
-            set -U fish_color_command             brgreen
-            set -U fish_color_comment             brmagenta
-            set -U fish_color_cwd                 green
-            set -U fish_color_cwd_root            red
-            set -U fish_color_end                 brmagenta
-            set -U fish_color_error               brred
-            set -U fish_color_escape              brcyan
-            set -U fish_color_history_current     --bold
-            set -U fish_color_host                normal
-            set -U fish_color_match               --background=brblue
-            set -U fish_color_normal              normal
-            set -U fish_color_operator            cyan
-            set -U fish_color_param               brblue
-            set -U fish_color_quote               yellow
-            set -U fish_color_redirection         bryellow
-            set -U fish_color_search_match        'bryellow' '--background=brblack'
-            set -U fish_color_selection           'white' '--bold' '--background=brblack'
-            set -U fish_color_status              red
-            set -U fish_color_user                brgreen
-            set -U fish_color_valid_path          --underline
-            set -U fish_pager_color_completion    normal
-            set -U fish_pager_color_description   yellow
-            set -U fish_pager_color_prefix        'white' '--bold' '--underline'
-            set -U fish_pager_color_progress      'brwhite' '--background=cyan'
-
-            function print_fish_colors --description 'Shows the various fish colors being used'
-              set -l clr_list (set -n | grep fish | grep color | grep -v __)
-              if test -n "$clr_list"
-                set -l bclr (set_color normal)
-                set -l bold (set_color --bold)
-                printf "\n| %-38s | %-38s |\n" Variable Definition
-                echo '|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|'
-                for var in $clr_list
-                  set -l def $$var
-                  set -l clr (set_color $def ^/dev/null)
-                  or begin
-                    printf "| %-38s | %s%-38s$bclr |\n" "$var" (set_color --bold white --background=red) "$def"
-                    continue
-                  end
-                  printf "| $clr%-38s$bclr | $bold%-38s$bclr |\n" "$var" "$def"
-                end
-                echo '|________________________________________|________________________________________|'\n
-              end
-            end
-          '';
+          "fish/config.fish".text = lib.mkAfter (builtins.readFile ./dotfiles/config.fish);
         };
       };
 
