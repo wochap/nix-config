84b6687f4c7f073fbe51b0adb2734322b4e3f3cf
wochap <gean.marroquin@gmail.com>
2021-02-11 19:12:41 -0500

    t

--- devices/config/users/config/dotfiles/dunstrc.nix
+++ devices/config/users/config/dotfiles/dunstrc.nix
@@ -1,10 +1,5 @@
 {
   global = {
-    title = "Dunst";
-    class = "Dunst";
-    monitor = 0;
-    follow = "mouse";
-
     # The geometry of the window. Format: [{width}]x{height}[+/-{x}+/-{y}]
     # The height = number of notifications, all other variables are px
     # Omit width, provide height for full-screen width notifications
@@ -13,75 +8,71 @@
     # Positive y value is measured from the top of the screen
     geometry = "500x6-16+72";
 
-    indicate_hidden = "yes";
-    shrink = "yes";
-
-    transparency = 0;
-    separator_height = 2;
-    padding = 8;
-    horizontal_padding = 13;
-    frame_width = 8;
-    frame_color = "#1C1E27";
-    separator_color = "#404859";
-
+    alignment = "center";
+    always_run_script = true;
+    browser = "firefox";
+    class = "Dunst";
+    corner_radius = 0;
+    follow = "mouse";
     font = "Roboto Bold 11";
-
+    format = "<span size='x-large' font_desc='Roboto 11' weight='bold' foreground='#B367CF'>%s</span>%b";
+    frame_color = "#1C1E27";
+    frame_width = 8;
+    hide_duplicate_count = true;
+    history_length = 20;
+    horizontal_padding = 13;
+    icon_position = "left";
+    idle_threshold = 120;
+    ignore_newline = false;
+    indicate_hidden = true;
     line_height = 0;
     markup = "full";
-    format = "<span size='x-large' font_desc='Roboto 11' weight='bold' foreground='#B367CF'>%s</span>\n\n%b";
-    alignment = "center";
-
-    idle_threshold = 120;
-    show_age_threshold = 60;
-    sort = "no";
-    word_wrap = "yes";
-    ignore_newline = "no";
-    stack_duplicates = false;
-    hide_duplicate_count = "yes";
-    show_indicators = "no";
-    sticky_history = "no";
-    history_length = 20;
-    always_run_script = true;
-    corner_radius = 0;
-    icon_position = "left";
     max_icon_size = 96;
     min_icon_size = 64;
-
-    browser = "firefox";
-
+    monitor = 0;
     mouse_left_click = "close_current";
     mouse_middle_click = "do_action";
     mouse_right_click = "close_all";
+    padding = 8;
+    separator_color = "#404859";
+    separator_height = 2;
+    show_age_threshold = 60;
+    show_indicators = false;
+    shrink = true;
+    sort = false;
+    stack_duplicates = false;
+    sticky_history = false;
+    title = "Dunst";
+    transparency = 0;
+    word_wrap = true;
   };
+  play_sound = {
+    script = "/etc/play_notification.sh";
+    summary = "*";
+  };
+  # espeak = {
+  #   script = "/etc/play_notification.sh";
+  #   summary = "*";
+  # };
   shortcuts = {
     close = "ctrl+space";
     close_all = "ctrl+shift+space";
-    history = "ctrl+Escape";
     context = "ctrl+shift+period";
+    history = "ctrl+Escape";
   };
   urgency_low = {
-    # IMPORTANT: colors have to be defined in quotation marks.
-    # Otherwise the "#" and following would be interpreted as a comment.
     background = "#1C1E27";
     foreground = "#cacacc";
-    timeout = 5 ;
-    # Icon for notifications with low urgency, uncomment to enable
-    #icon = /path/to/icon;
+    timeout = 5;
   };
   urgency_normal = {
     background = "#1C1E27";
     foreground = "#cacacc";
     timeout = 5;
-    # Icon for notifications with normal urgency, uncomment to enable
-    #icon = /path/to/icon;
   };
   urgency_critical = {
     background = "#1C1E27";
     foreground = "#cacacc";
     timeout = 120;
-  };
-  play_sound = {
-    summary = "*";
-    script = "/etc/play_notification.sh";
   };
 }
