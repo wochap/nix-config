3924e9b65ec6e9afc32ffbeb47f65aa6c573c35e
wochap <gean.marroquin@gmail.com>
2021-02-27 13:40:21 -0500

    fix slowness of pastes

--- devices/config/users/config/zsh.nix
+++ devices/config/users/config/zsh.nix
@@ -10,6 +10,20 @@
         enable = true;
         dotDir = ".config/zsh";
         initExtra = ''
+          ### Fix slowness of pastes with zsh-syntax-highlighting.zsh
+          ### https://github.com/zsh-users/zsh-autosuggestions/issues/238
+          pasteinit() {
+            OLD_SELF_INSERT=''${''${(s.:.)widgets[self-insert]}[2,3]}
+            zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
+          }
+
+          pastefinish() {
+            zle -N self-insert $OLD_SELF_INSERT
+          }
+          zstyle :bracketed-paste-magic paste-init pasteinit
+          zstyle :bracketed-paste-magic paste-finish pastefinish
+          ### Fix slowness of pastes
+
           source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
         '';
         enableCompletion = true;
