644c413908d5130e5c970d4107e52b0b0c0918dc
wochap <gean.marroquin@gmail.com>
2021-03-03 08:05:58 -0500

    fix

--- devices/config/scripts/sxhkd-help.sh
+++ devices/config/scripts/sxhkd-help.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 awk '/^[a-z]/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' /etc/sxhkdrc |
-  column -t -s $'\t' |
+  column -t -s $'\t' | \
   rofi \
     -dmenu \
     -i \
     -markup-rows \
     -no-show-icons \
     -theme /home/gean/.config/rofi-theme.rasi \
-    -theme-str 'window \{ width: 35em; \}'
+    -theme-str 'window { width: 35em; }' \
     -lines 15 \
     -yoffset 40
