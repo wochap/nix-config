79d15b00c2586e7d35e132ab5c93b35b3ffd812b
wochap <gean.marroquin@gmail.com>
2021-03-05 09:34:39 -0500

    fix

--- devices/config/packages/default.nix
+++ devices/config/packages/default.nix
@@ -9,23 +9,3 @@
   zscroll = pkgs.callPackage ./zscroll {};
   lightdm-webkit2-greeter = pkgs.callPackage ./lightdm-webkit2-greeter {};
 }
-
-# { pkgs }:
-
-# let nurPkgs = (mergedPkgs:
-#   let
-#     callPackage = pkgs.newScope mergedPKgs;
-#   in
-#   with mergedPkgs;
-#   {
-#     inherit callPackage;
-#     eww = pkgs.callPackage ./eww {};
-#     horizon-icons = pkgs.callPackage ./horizon-icons {};
-#     horizon-theme = pkgs.callPackage ./horizon-theme {};
-#     whitesur-dark-theme = pkgs.callPackage ./whitesur-dark-theme {};
-#     whitesur-dark-icons = pkgs.callPackage ./whitesur-dark-icons {};
-#     zscroll = pkgs.callPackage ./zscroll {};
-#     lightdm-webkit2-greeter = pkgs.callPackage ./lightdm-webkit2-greeter {};
-#   }
-# ) (pkgs);
-# in nurPkgs
