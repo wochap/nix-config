80c7310043ac4a18cfd1195e705d216769db9d49
wochap <gean.marroquin@gmail.com>
2021-03-07 15:51:51 -0500

    firefox

--- devices/config/users/config/firefox.nix
+++ devices/config/users/config/firefox.nix
@@ -15,6 +15,13 @@ in
       home.file = {
         ".mozilla/firefox/default/chrome/userChrome.css".source = "${firefox-theme}/chrome/userChrome.css";
         ".mozilla/firefox/default/chrome/WhiteSur".source = "${firefox-theme}/chrome/WhiteSur";
+        ".mozilla/firefox/default/chrome/customChrome.css".text = ''
+          /* Remove Tab outline */
+          .keyboard-focused-tab > .tab-stack > .tab-content,
+          .tabbrowser-tab:focus:not([aria-activedescendant]) > .tab-stack > .tab-content {
+            outline: 0 !important;
+          }
+        '';
       };
       programs.firefox = {
         enable = true;
@@ -25,6 +32,7 @@ in
             name = "default";
             isDefault = true;
             settings = {
+              "gfx.webrender.all" = true;
               "browser.tabs.tabMinWidth" = 5;
               "ui.context_menus.after_mouseup" = true;
               "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
