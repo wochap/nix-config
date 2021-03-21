16580127f817ceaafd3415f4f59857a03bc07aee
wochap <gean.marroquin@gmail.com>
2021-03-21 08:48:31 -0500

    t

--- devices/config/darwin.nix
+++ devices/config/darwin.nix
@@ -16,6 +16,24 @@ in
     ./users/gean.nix
   ];
 
+  # https://discourse.nixos.org/t/using-mkif-with-nested-if/5221/4
+  # https://discourse.nixos.org/t/best-resources-for-learning-about-the-nixos-module-system/1177/4
+  # https://nixos.org/manual/nixos/stable/index.html#sec-option-types
+  options = {
+    _displayServer = lib.mkOption {
+      type = lib.types.str;
+      default = "";
+      example = "xorg"; # xorg, wayland
+      description = "Display server type, used by common config files.";
+    };
+    _isHidpi = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      example = true;
+      description = "Flag for hidpi displays.";
+    };
+  };
+
   config = {
     _displayServer = "darwin";
 
