2af2d76ceae7033da2d45977f103a1734f414bf0
wochap <gean.marroquin@gmail.com>
2021-03-21 10:38:13 -0500

    lorri

--- devices/config/darwin.nix
+++ devices/config/darwin.nix
@@ -38,6 +38,29 @@ in
     # Allow unfree packages
     nixpkgs.config.allowUnfree = true;
 
+    environment.systemPackages = with pkgs; [
+      kitty # terminal
+      lorri
+    ];
+
+    # XXX: Copied verbatim from https://github.com/iknow/nix-channel/blob/7bf3584e0bef531836050b60a9bbd29024a1af81/darwin-modules/lorri.nix
+    launchd.user.agents = {
+      "lorri" = {
+        serviceConfig = {
+          WorkingDirectory = (builtins.getEnv "HOME");
+          EnvironmentVariables = { };
+          KeepAlive = true;
+          RunAtLoad = true;
+          StandardOutPath = "/var/tmp/lorri.log";
+          StandardErrorPath = "/var/tmp/lorri.log";
+        };
+        script = ''
+          source ${config.system.build.setEnvironment}
+          exec ${pkgs.lorri}/bin/lorri daemon
+        '';
+      };
+    };
+
     environment = {
       etc = {
         "open_url.sh" = {
