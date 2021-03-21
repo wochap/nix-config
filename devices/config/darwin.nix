592219e9ea81a1d66db25192b4aa6aab080b8d41
wochap <gean.marroquin@gmail.com>
2021-03-21 15:28:57 -0500

    t

--- devices/config/darwin.nix
+++ devices/config/darwin.nix
@@ -122,12 +122,37 @@ in
 
       };
       extraConfig = ''
+        yabai -m config focus_follows_mouse autofocus
+        yabai -m config window_topmost on
+        yabai -m config window_shadow float
+        yabai -m config window_border on
+        yabai -m config window_border_width 2
+        yabai -m config active_window_border_color \#2C4E9C
+        yabai -m config normal_window_border_color \#282e3a
+        yabai -m config split_ratio 0.50
+        yabai -m config mouse_modifier cmd
+        yabai -m config mouse_action1 move
+        yabai -m config mouse_action2 resize
+        yabai -m config mouse_drop_action swap
       '';
     };
 
     services.skhd = {
       enable = true;
       skhdConfig = ''
+        # Focus desktop
+        cmd - 1  : yabai -m display --focus 1
+        cmd - 2  : yabai -m display --focus 2
+        cmd - 3  : yabai -m display --focus 3
+        cmd - 4  : yabai -m display --focus 4
+
+        # Send window to desktop
+        cmd + shift - 1  : yabai -m window --space 1
+        cmd + shift - 2  : yabai -m window --space 2
+        cmd + shift - 3  : yabai -m window --space 3
+        cmd + shift - 4  : yabai -m window --space 4
+
+        cmd - f : yabai -m window --toggle zoom-fullscreen
       '';
     };
   };
