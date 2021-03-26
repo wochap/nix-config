15bf8518dcf3f0f545953478e287b35ade756b0c
wochap <gean.marroquin@gmail.com>
2021-03-26 17:04:15 -0500

    refactor

--- devices/config/users/config/default.nix
+++ devices/config/users/config/default.nix
@@ -13,6 +13,7 @@ in
     ./mixins/picom.nix
     ./mixins/polybar.nix
     ./mixins/zsh.nix
+    ./mixins/mime-apps.nix
   ];
 
   config = {
@@ -65,40 +66,7 @@ in
       };
 
       # Edit home files
-      xdg = {
-        enable = true;
-
-        # Control default apps
-        mimeApps.enable = true;
-        mimeApps.defaultApplications = {
-          "application/pdf" = [ "org.pwmt.zathura.desktop" ];
-          "image/jpeg" = [ "org.gnome.eog.desktop" ];
-          "image/png" = [ "org.gnome.eog.desktop" ];
-          "image/svg+xml" = [ "org.gnome.eog.desktop" ];
-          "inode/directory" = [ "thunar.desktop" ];
-          "message/rfc822" = [ "userapp-Thunderbird-LAA0Y0.desktop" ];
-          "text/html" = [ "google-chrome.desktop" ];
-          "video/mp4" = [ "mpv.desktop" ];
-          "video/x-matroska" = [ "mpv.desktop" ];
-          "x-scheme-handler/http" = [ "http-url-handler.desktop" ];
-          "x-scheme-handler/https" = [ "http-url-handler.desktop" ];
-          "x-scheme-handler/mailto" = [ "exo-mail-reader.desktop" ];
-          "x-scheme-handler/webcal" = [ "google-chrome.desktop" ];
-        };
-        mimeApps.associations.added = {
-          "application/pdf" = [ "org.pwmt.zathura.desktop" ];
-          "image/jpeg" = [ "org.gnome.eog.desktop" "org.nomacs.ImageLounge.desktop" ];
-          "image/png" = [ "org.gnome.eog.desktop" "org.nomacs.ImageLounge.desktop" ];
-          "image/svg+xml" = [ "org.gnome.eog.desktop" ];
-          "message/rfc822" = [ "userapp-Thunderbird-LAA0Y0.desktop" ];
-          "text/html" = [ "http-url-handler.desktop" "firefox.desktop" "google-chrome.desktop" "brave-browser.desktop" ];
-          "video/mp4" = [ "mpv.desktop" ];
-          "video/x-matroska" = [ "mpv.desktop" ];
-          "x-scheme-handler/http" = [ "http-url-handler.desktop" "firefox.desktop" "google-chrome.desktop" "brave-browser.desktop" ];
-          "x-scheme-handler/https" = [ "http-url-handler.desktop" "firefox.desktop" "google-chrome.desktop" "brave-browser.desktop" ];
-          "x-scheme-handler/mailto" = [ "userapp-Thunderbird-LAA0Y0.desktop" "userapp-Evolution-S7FTY0.desktop" ];
-        };
-      };
+      xdg.enable true;
 
       # Add config files to home folder
       home.file = lib.mkMerge [
