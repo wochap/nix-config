bee45ffad5754b9ae80bb6eac89a6c2f0749396d
wochap <gean.marroquin@gmail.com>
2021-03-26 10:07:33 -0500

    discord

--- devices/config/common.nix
+++ devices/config/common.nix
@@ -85,6 +85,21 @@ in
     # Explicit PulseAudio support in applications
     nixpkgs.config.pulseaudio = true;
 
+    # Update discord to latest version
+    # https://nixos.wiki/wiki/Discord
+    nixpkgs.overlays = [
+      (self: super: {
+        discord = super.discord.overrideAttrs (
+          _: {
+            src = pkgs.fetchurl {
+              url = "https://dl.discordapp.net/apps/linux/0.0.14/discord-0.0.14.tar.gz";
+              sha256 = "1rq490fdl5pinhxk8lkfcfmfq7apj79jzf3m14yql1rc9gpilrf2";
+            };
+          }
+        );
+      })
+    ];
+
     # Set your time zone.
     time.timeZone = "America/Lima";
 
