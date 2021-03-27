663705faaee8d1b98c262c05ca13550f804f63e3
wochap <gean.marroquin@gmail.com>
2021-03-27 08:08:36 -0500

    refactor

--- devices/config/users/gean-darwin.nix
+++ devices/config/users/gean-darwin.nix
@@ -6,6 +6,12 @@
   ];
 
   config = {
+    users.users.gean = {
+      shell = pkgs.zsh;
+      uid = 1000;
+      home = "/Users/gean";
+    };
+
     home-manager.users.gean = {
       # Home Manager needs a bit of information about you and the
       # paths it should manage.
