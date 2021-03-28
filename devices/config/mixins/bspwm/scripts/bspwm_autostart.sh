31bcd33333466f86e5bb070c68571ff3b2089471
wochap <gean.marroquin@gmail.com>
2021-03-28 10:38:09 -0500

    rice alttab

--- devices/config/mixins/bspwm/scripts/bspwm_autostart.sh
+++ devices/config/mixins/bspwm/scripts/bspwm_autostart.sh
@@ -8,8 +8,8 @@ function run {
 }
 
 nitrogen --restore &
-alttab -w 1 -d 0 -sc 1 -i 199x64 -t 200x200 -s 3 -bg "#0d1117" -fg "#c9d1d9" -frame "#58a6ff" -font "xft:Iosevka:weight=bold:size=12" -theme "WhiteSur-dark" -mk Super_L &
-alttab -w 1 -d 1 -sc 1 -i 199x64 -t 200x200 -s 3 -bg "#0d1117" -fg "#c9d1d9" -frame "#58a6ff" -font "xft:Iosevka:weight=bold:size=12" -theme "WhiteSur-dark" &
+alttab -w 1 -d 0 -sc 1 -i 224x128 -t 225x225 -s 3 -bg "#0d1117" -fg "#c9d1d9" -frame "#58a6ff" -font "xft:Iosevka:weight=bold:size=12" -theme "WhiteSur-dark" -mk Super_L &
+alttab -w 1 -d 1 -sc 1 -i 224x128 -t 225x225 -s 3 -bg "#0d1117" -fg "#c9d1d9" -frame "#58a6ff" -font "xft:Iosevka:weight=bold:size=12" -theme "WhiteSur-dark" &
 blueberry-tray &
 clipmenud &
 xsetroot -cursor_name left_ptr & # fix cursor icon
