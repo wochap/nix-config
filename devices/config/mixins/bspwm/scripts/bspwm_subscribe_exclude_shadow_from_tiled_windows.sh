6b6b13c693bea895b930bdca3ce118eb51a7892e
wochap <gean.marroquin@gmail.com>
2021-06-17 19:18:17 -0500

    restore shadow on rofi and dunst

--- devices/config/mixins/bspwm/scripts/bspwm_subscribe_exclude_shadow_from_tiled_windows.sh
+++ devices/config/mixins/bspwm/scripts/bspwm_subscribe_exclude_shadow_from_tiled_windows.sh
@@ -1,15 +1,8 @@
 bspc subscribe node_state | while read -r _ _ _ node state status; do
-  case "$(xprop -id "$node" _NET_WM_STATE)" in
-    *_NET_WM_STATE_ABOVE* )
-      echo "Do nothing"
-    ;;
-    * )
-      if [[ "$state" == "floating" ]]; then
-        case "$status" in
-          off) xprop -id "$node" -remove _PICOM_SHADOW;;
-          on) xprop -id "$node" -f _PICOM_SHADOW 32c -set _PICOM_SHADOW 1;;
-        esac
-      fi
-    ;;
-  esac;;
+  if [[ "$state" == "floating" ]]; then
+    case "$status" in
+      off) xprop -id "$node" -remove _PICOM_SHADOW;;
+      on) xprop -id "$node" -f _PICOM_SHADOW 32c -set _PICOM_SHADOW 1;;
+    esac
+  fi
 done
