aedb69900d23a95502d32e4a88fac345875f6339
wochap <gean.marroquin@gmail.com>
2021-06-19 09:11:22 -0500

    test

--- devices/config/mixins/bspwm/scripts/bspwm_subscribe_exclude_shadow_from_tiled_windows.sh
+++ devices/config/mixins/bspwm/scripts/bspwm_subscribe_exclude_shadow_from_tiled_windows.sh
@@ -6,3 +6,18 @@ bspc subscribe node_state | while read -r _ _ _ node state status; do
     esac
   fi
 done
+
+# bspc subscribe node_focus | while read -r _ _ _ focused_node; do
+#   floating_nodes=$(bspc query --nodes --node .floating)
+#   nodes=$(bspc query --nodes)
+#   for node in $nodes
+#   do
+#     is_node_floating=$([[ " ${floating_nodes[@]} " =~ " ${node} " ]] && echo "true" || echo "false")
+#     if [[ $is_node_floating == "true" || $node == $focused_node  ]]
+#     then
+#       xprop -id "$node" -f _PICOM_SHADOW 32c -set _PICOM_SHADOW 1
+#     else
+#       xprop -id "$node" -remove _PICOM_SHADOW
+#     fi
+#   done
+# done
