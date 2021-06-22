e08cb1f6f48bdc5a72c64e1883ff48355682b698
wochap <gean.marroquin@gmail.com>
2021-06-22 10:34:32 -0500

    add doge and btc status

--- devices/config/users/config/mixins/polybar/default.nix
+++ devices/config/users/config/mixins/polybar/default.nix
@@ -46,6 +46,14 @@ in
           source = ./dotfiles/scripts/rextie_usd.js;
           executable = true;
         };
+        ".config/polybar/scripts/btc_usd.js" = {
+          source = ./dotfiles/scripts/btc_usd.js;
+          executable = true;
+        };
+        ".config/polybar/scripts/doge_usd.js" = {
+          source = ./dotfiles/scripts/doge_usd.js;
+          executable = true;
+        };
       };
       services.polybar = {
         enable = true;
