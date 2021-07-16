e90348808110c21c1a241da40af444f67a7f2733
wochap <gean.marroquin@gmail.com>
2021-07-16 18:55:45 -0500

    rice polybar

--- devices/config/mixins/bspwm/scripts/bspwm_autostart.sh
+++ devices/config/mixins/bspwm/scripts/bspwm_autostart.sh
@@ -7,12 +7,17 @@ function run {
   fi
 }
 
-alttab -w 1 -d 0 -sc 1 -i 224x128 -t 225x225 -s 3 -bg "#0d1117" -fg "#c9d1d9" -frame "#ff1901" -font "xft:Inter:weight=light:size=11" -theme "WhiteSur-dark" -mk Super_L &
-alttab -w 1 -d 1 -sc 1 -i 224x128 -t 225x225 -s 3 -bg "#0d1117" -fg "#c9d1d9" -frame "#ff1901" -font "xft:Inter:weight=light:size=11" -theme "WhiteSur-dark" &
+alttab -w 1 -d 0 -sc 1 -i 224x128 -t 225x225 -s 3 -bg "#0d1117" -fg "#c9d1d9" -frame "#ff1901" -font "xft:Inter:weight=regular:size=11" -theme "WhiteSur-dark" -mk Super_L &
+alttab -w 1 -d 1 -sc 1 -i 224x128 -t 225x225 -s 3 -bg "#0d1117" -fg "#c9d1d9" -frame "#ff1901" -font "xft:Inter:weight=regular:size=11" -theme "WhiteSur-dark" &
 blueberry-tray &
 clipmenud &
 xsetroot -cursor_name left_ptr & # fix cursor icon
 eww daemon &
 caffeine &
-polybar main -r &
+polybar powermenu -q -r &
+polybar workspaces -q -r &
+polybar tray -q -r &
+polybar right -q -r &
+polybar xkeyboard -q -r &
+polybar monocle -q-indicator -r &
 sh /etc/fix_caps_lock_delay.sh &
