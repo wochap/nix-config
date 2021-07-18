12351af5b0d421a78510ec07361444e4c62c08ec
wochap <gean.marroquin@gmail.com>
2021-07-18 11:17:43 -0500

    refactor

--- devices/config/users/config/mixins/polybar/dotfiles/scripts/polybar-start.sh
+++ devices/config/users/config/mixins/polybar/dotfiles/scripts/polybar-start.sh
@@ -4,5 +4,5 @@ polybar powermenu -q -r &
 polybar workspaces -q -r &
 # polybar tray -q -r &
 polybar right -q -r &
-polybar xkeyboard -q -r &
+# polybar xkeyboard -q -r &
 polybar monocle-indicator -q -r &
