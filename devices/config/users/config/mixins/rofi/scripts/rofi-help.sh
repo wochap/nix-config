7b69710db4887b2fdf93232dcb2c4b09d6fce7da
wochap <gean.marroquin@gmail.com>
2021-07-20 18:01:41 -0500

    🍙
    * update polybar right module, add month, month number and day name
    * fix rofi-help.sh
    * remove rounded corner on normal windows

--- devices/config/users/config/mixins/rofi/scripts/rofi-help.sh
+++ devices/config/users/config/mixins/rofi/scripts/rofi-help.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-awk '/^[a-z]/ && last {print "<span>",$0,"\t",last,"</span>"} {last=""} /^#/{last=$0}' /etc/sxhkdrc |
+awk '/^[a-z]/ && last {print "<span>",$0,"\t",last,"</span>"} {last=""} /^#/{last=$0}' /etc/config/sxhkdrc |
   column -t -s $'\t' | \
   rofi \
     -dmenu \
