c2e237860add6e455027601580f970f6a97aaa03
wochap <gean.marroquin@gmail.com>
2021-07-25 13:38:53 -0500

    powermenu

--- devices/config/users/config/mixins/rofi/scripts/rofi-powermenu.sh
+++ devices/config/users/config/mixins/rofi/scripts/rofi-powermenu.sh
@@ -1,36 +1,22 @@
 #!/usr/bin/env bash
 
-#
-# Powermenu for Rofi
-#
-# Author : Lucero Alvarado
-# Github : @lu0
-#
-
 # Options as characters
 # Copied from decoded unicodes (private use of "Feather" font)
-shutdown="襤";        # "\uf924"
-reboot="ﰇ";          # "\ufc07"
-sleep="";           # "\uf186"
-logout="";          # "\uf842"
-lock="";            # "\uf840"
-# shutdown="";        # "\uE9C0"
-# reboot="";          # "\uE9C4"
-# sleep="";           # "\uE9A3"
-# logout="";          # "\uE991"
-# lock="";            # "\uE98F"
+shutdown="襤"; # "\uf924"
+reboot="ﰇ"; # "\ufc07"
+sleep=""; # "\uf186"
+logout=""; # "\uf842"
+lock=""; # "\uf840"
+# shutdown=""; # "\uE9C0"
+# reboot=""; # "\uE9C4"
+# sleep=""; # "\uE9A3"
+# logout=""; # "\uE991"
+# lock=""; # "\uE98F"
 options="$shutdown\n$reboot\n$sleep\n$logout\n$lock"
-
-# Fake blurred background
-# SS_PATH="$HOME/.config/rofi/screenshot"
-# rm -f "${SS_PATH}.jpg" && scrot -z "${SS_PATH}.jpg"                 # screenshot
-# convert "${SS_PATH}.jpg" -blur 0x10 -auto-level "${SS_PATH}.jpg"    # blur
-# convert "${SS_PATH}.jpg" "${SS_PATH}.png"                           # rofi reads png images
 
 # Font size according to screen dimensions
 DEFAULT_WIDTH=1920
 WIDTH=$(xrandr | grep primary | awk '{print $4}' | cut -d 'x' -f 1)
-# WIDTH=$(xdpyinfo | grep dimensions | awk '{print $2}' | cut -d 'x' -f 1 )
 DEFAULT_FONTSIZE=50
 FONTSIZE=$(echo "$WIDTH/$DEFAULT_WIDTH*$DEFAULT_FONTSIZE" | bc -l)
 
@@ -41,18 +27,17 @@ while getopts "lp" OPT; do
     *) PRESELECTION=4 ;;
   esac
 done
+
 if (( $OPTIND == 1 )); then
   PRESELECTION=4
 fi
 
-# -fake-background ${SS_PATH}.png \
-# -fake-transparency \
 selected="$(echo -e "$options" |
-            rofi -theme /etc/config/rofi-powermenu-theme.rasi \
-              -font "Iosevka, $FONTSIZE" \
-              -p "See you later, ${LOGNAME^}!" \
-              -m "primary" \
-              -dmenu -selected-row ${PRESELECTION})"
+  rofi -theme /etc/config/rofi-powermenu-theme.rasi \
+    -font "Iosevka, $FONTSIZE" \
+    -p "See you later, ${LOGNAME^}!" \
+    -m "primary" \
+    -dmenu -selected-row ${PRESELECTION})"
 
 case $selected in
   $shutdown)
