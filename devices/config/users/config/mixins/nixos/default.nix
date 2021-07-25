f74126931efe57f39b672b17aa20afef2e76686d
wochap <gean.marroquin@gmail.com>
2021-07-25 14:26:07 -0500

    refactor

--- devices/config/users/config/mixins/nixos/default.nix
+++ devices/config/users/config/mixins/nixos/default.nix
@@ -1,5 +1,8 @@
 { config, pkgs, lib, ... }:
 
+let
+  isWayland = config._displayServer == "wayland";
+in
 {
   config = {
     home-manager.users.gean = {
@@ -49,6 +52,17 @@
         enableSshSupport = true;
         defaultCacheTtl = 1800;
         pinentryFlavor = "gnome3";
+      };
+
+      services.redshift = {
+        enable = true;
+        package = if isWayland then pkgs.redshift-wlr else pkgs.redshift;
+        latitude = "-12.051408";
+        longitude = "-76.922124";
+        temperature = {
+          day = 4000;
+          night = 3700;
+        };
       };
     };
   };
--- devices/config/users/config/xorg.nix
+++ devices/config/users/config/xorg.nix
@@ -40,16 +40,6 @@ in
         "Xcursor.size" = 40;
       };
 
-      services.redshift = {
-        enable = true;
-        latitude = "-12.051408";
-        longitude = "-76.922124";
-        temperature = {
-          day = 4000;
-          night = 3700;
-        };
-      };
-
       # screenshot utility
       services.flameshot.enable = true;
     };
