dbbaf7250e5873cde1c3dec28b0e0bcdb46b8058
wochap <gean.marroquin@gmail.com>
2021-09-09 11:41:58 -0500

    fix typo

--- devices/desktop.nix
+++ devices/desktop.nix
@@ -30,17 +30,17 @@ in
     # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
     system.stateVersion = "20.09"; # Did you read the comment?
 
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
-    home-manager.users.${userName}.home.stateVersion = "21.03";
-
     home-manager.users.${userName} = {
+      # This value determines the Home Manager release that your
+      # configuration is compatible with. This helps avoid breakage
+      # when a new Home Manager release introduces backwards
+      # incompatible changes.
+      #
+      # You can update Home Manager without changing this value. See
+      # the Home Manager release notes for a list of state version
+      # changes in each release.
+      home.stateVersion = "21.03";
+
       xresources.properties = {
         "Xft.dpi" = dpi;
       };
