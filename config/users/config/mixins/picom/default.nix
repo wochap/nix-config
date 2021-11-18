c00b2ac8147851b101a8364d34ec1bad941b45bb
wochap <gean.marroquin@gmail.com>
2021-11-18 12:31:19 -0500

    update picom

--- config/users/config/mixins/picom/default.nix
+++ config/users/config/mixins/picom/default.nix
@@ -14,7 +14,7 @@ in
         vSync = true;
         backend = "glx";
         # Disabling experimentalBackends, removes corners artifact with shadows
-        experimentalBackends = false;
+        experimentalBackends = true;
 
         extraOptions = (builtins.readFile ./dotfiles/picom.conf);
         package = pkgs.picom.overrideAttrs(o: {
@@ -23,8 +23,8 @@ in
             # repo = "picom";
             # owner = "yshui";
             # # next branch
-            # rev = "fd381dacff5c755b43effba441fb5706a0f378a4";
-            # sha256 = "0za3ywdn27dzp7140cpg1imbqpbflpzgarr76xaqmijz97rv1909";
+            # rev = "6555703b03fed197f6ff7fe6ddbd5a249c253ac3";
+            # sha256 = "sha256-WDx5WCND1UGszaAILbk+bU0VN9/mCLLZ9evHIbUi1B8=";
 
             # # Stable picom
             # repo = "picom";
@@ -33,19 +33,12 @@ in
             # rev = "dac85eac10082dfc3df463aaa74b811144e22122ht";
             # sha256 = "0gjksayz2xpmgglvw17ppsan2imrd1fijs579kbf27xwp503xgfl";
 
-            # # Least screen tearing but... artifacts
-            # repo = "picom";
-            # owner = "ibhagwan";
-            # # next branch
-            # rev = "0539616510d9ad339f1af685e9ee39183a8f3562";
-            # sha256 = "1z51xk9vdy5l925msmprmhzjis516h76id2qhdswf916ssjgxl6m";
-
             # Aceptable fix for screen tearing but... artifacts+ (WORKS)
             repo = "picom";
             owner = "ibhagwan";
             # next-rebase branch
-            rev = "60eb00ce1b52aee46d343481d0530d5013ab850b";
-            sha256 = "1m17znhl42sa6ry31yiy05j5ql6razajzd6s3k2wz4c63rc2fd1w";
+            rev = "c4107bb6cc17773fdc6c48bb2e475ef957513c7a";
+            sha256 = "sha256-1hVFBGo4Ieke2T9PqMur1w4D0bz/L3FAvfujY9Zergw=";
           };
         });
       };
