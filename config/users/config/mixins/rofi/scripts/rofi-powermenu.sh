1abbc2280de13dc99776e0cc8fb3c64e6fb93c33
wochap <gean.marroquin@gmail.com>
2021-11-21 17:43:36 -0500

    clean

--- config/users/config/mixins/rofi/scripts/rofi-powermenu.sh
+++ config/users/config/mixins/rofi/scripts/rofi-powermenu.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-# Options as characters
-# Copied from decoded unicodes (private use of "Feather" font)
 shutdown="襤"; # "\uf924"
 reboot="ﰇ"; # "\ufc07"
 sleep=""; # "\uf186"
+hibernate=""; # "\uf186"
 logout=""; # "\uf842"
 lock=""; # "\uf840"
-# shutdown=""; # "\uE9C0"
-# reboot=""; # "\uE9C4"
-# sleep=""; # "\uE9A3"
-# logout=""; # "\uE991"
-# lock=""; # "\uE98F"
 options="$shutdown\n$reboot\n$sleep\n$logout\n$lock"
 
 # Font size according to screen dimensions
@@ -19,41 +13,36 @@ DEFAULT_WIDTH=1920
 WIDTH=$(xrandr | grep primary | awk '{print $4}' | cut -d 'x' -f 1)
 DEFAULT_FONTSIZE=50
 FONTSIZE=$(echo "$WIDTH/$DEFAULT_WIDTH*$DEFAULT_FONTSIZE" | bc -l)
+PRESELECTION=4
 
-while getopts "lp" OPT; do
-  case "$OPT" in
-    p) PRESELECTION=0 ;;
-    l) PRESELECTION=3 ;;
-    *) PRESELECTION=4 ;;
-  esac
-done
-
-if (( $OPTIND == 1 )); then
-  PRESELECTION=4
-fi
-
-selected="$(echo -e "$options" |
-  rofi -theme /etc/config/rofi-powermenu-theme.rasi \
-    -font "Iosevka, $FONTSIZE" \
-    -m "primary" \
-    -dmenu -selected-row ${PRESELECTION})"
+selected="$(echo -e "$options" | rofi -theme /etc/config/rofi-powermenu-theme.rasi \
+                                  -font "Iosevka, $FONTSIZE" \
+                                  -m "primary" \
+                                  -dmenu -selected-row ${PRESELECTION})"
 
 case $selected in
   $shutdown)
-    systemctl poweroff
+    exec systemctl poweroff --check-inhibitors=no
     ;;
   $reboot)
-    systemctl reboot
+    exec systemctl reboot
+    ;;
+  $hibernate)
+    # Hold all on disk
+    exec systemctl hibernate
     ;;
   $sleep)
+    # Hold all on RAM
     mpc -q pause
     amixer set Master mute
-    systemctl suspend
+    exec systemctl suspend
     ;;
   $logout)
-    bspc quit
+    # TODO: add condition for sway
+    # exec swaymsg exit
+    exec bspc quit
     ;;
   $lock)
-    /etc/scripts/lock.sh
+    exec /etc/scripts/lock.sh
     ;;
 esac
