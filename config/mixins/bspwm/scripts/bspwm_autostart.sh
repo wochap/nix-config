f806fb6493e470a949a5eb1f876fe997217b9503
wochap <gean.marroquin@gmail.com>
2021-11-21 14:20:39 -0500

    f

--- config/mixins/bspwm/scripts/bspwm_autostart.sh
+++ config/mixins/bspwm/scripts/bspwm_autostart.sh
@@ -8,8 +8,7 @@ function run {
 }
 
 # TODO: fix Kmonad lmet + tab
-# alttab -w 1 -d 0 -sc 1 -i 224x128 -t 225x225 -s 3 -bg "#282a36" -fg "#f8f8f2" -frame "#ff79c6" -font "xft:Inter:weight=Medium:size=11" -theme "WhiteSur-dark" -mk Super_L &
-alttab -w 1 -d 0 -sc 1 -i 224x128 -t 225x225 -s 3 -bg "#282a36" -fg "#f8f8f2" -frame "#ff79c6" -font "xft:Inter:weight=Medium:size=11" -theme "WhiteSur-dark" &
+alttab -w 1 -d 0 -sc 1 -i 224x128 -t 225x225 -s 3 -bg "#282a36" -fg "#f8f8f2" -frame "#ff79c6" -font "xft:Inter:weight=Medium:size=11" -theme "WhiteSur-dark" -mk Super_L &
 blueberry-tray &
 clipmenud &
 # eww daemon &
