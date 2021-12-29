c1a630047f40a8c99a869ff9b5b4f697b55008e7
wochap <gean.marroquin@gmail.com>
2021-12-29 07:25:06 -0500

    zsh

--- config/users/config/mixins/zsh.nix
+++ config/users/config/mixins/zsh.nix
@@ -8,6 +8,12 @@ let
     rev = "47ba26d2d4912a1b8de066e589633ff1963c5621";
     sha256 = "1rhvbaz2v8kcggvh3flj6ri2jry4wdz6xx5br91i36f5alc2vk1i";
   };
+  zsh-vim-mode = pkgs.fetchFromGitHub {
+    owner = "softmoth";
+    repo = "zsh-vim-mode";
+    rev = "1f9953b7d6f2f0a8d2cb8e8977baa48278a31eab";
+    sha256 = "sha256-a+6EWMRY1c1HQpNtJf5InCzU7/RphZjimLdXIXbO6cQ=";
+  };
 in
 {
   config = {
@@ -38,6 +44,11 @@ in
             PageUp     "''${terminfo[kpp]}"
             PageDown   "''${terminfo[knp]}"
           )
+
+          # ENABLE ZSH COMPLETION SYSTEM (OMZSH USED TO DO IT FOR US)
+          zmodload -i zsh/complist
+          autoload -U bashcompinit
+          bashcompinit
 
           function killport {
             kill $(lsof -t -i:"$1")
@@ -101,6 +112,7 @@ in
           # Sessions append to the history list in the order they exit.
           setopt APPEND_HISTORY
 
+          source ${zsh-vim-mode}/zsh-vim-mode.plugin.zsh
           source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
           source ${dracula-zsh-syntax-highlighting}/zsh-syntax-highlighting.sh
         '';
