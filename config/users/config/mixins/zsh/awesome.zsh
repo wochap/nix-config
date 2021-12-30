9268ee3a6d780d07c47d180a356c409689406fcf
wochap <gean.marroquin@gmail.com>
2021-12-30 07:11:36 -0500

    add fns

--- config/users/config/mixins/zsh/awesome.zsh
+++ config/users/config/mixins/zsh/awesome.zsh
@@ -1,8 +1,14 @@
-
+# cd into git repository
 function pro() {
-  projects=$(find ~/Projects -maxdepth 2 -type d -execdir test -d {}/.git \; -print -prune)
+  projects=$(find ~/.config -maxdepth 1 -type d -execdir test -d {}/.git \; -print -prune)
+  projects+=$(find ~/Projects -maxdepth 2 -type d -execdir test -d {}/.git \; -print -prune)
   projects+=$(find ~/ -maxdepth 1 -type d -execdir test -d {}/.git \; -print -prune)
 
   cd $(echo "$projects" | fzf)
 }
 
+# run npm script (requires jq)
+fns() {
+  local script
+  script=$(cat package.json | jq -r '.scripts | keys[] ' | sort | fzf) && npm run $(echo "$script")
+}
