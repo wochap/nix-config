813c963b80e358667b3dd4f9938d0f2aaeecc614
wochap <gean.marroquin@gmail.com>
2022-01-05 07:28:38 -0500

    feat: update keybindings

--- config/mixins/bspwm/scripts/bspwm_autostart.sh
+++ config/mixins/bspwm/scripts/bspwm_autostart.sh
@@ -8,7 +8,7 @@ function run {
 }
 
 # TODO: fix Kmonad lmet + tab
-alttab -w 1 -d 0 -sc 1 -i 224x128 -t 225x225 -s 3 -bg "#282a36" -fg "#f8f8f2" -frame "#ff79c6" -font "xft:Inter:weight=Medium:size=11" -theme "WhiteSur-dark" -mk Super_L &
+alttab -w 1 -d 0 -sc 1 -i 224x128 -t 225x225 -s 3 -bg "#282a36" -fg "#f8f8f2" -frame "#ff79c6" -font "xft:Inter:weight=Medium:size=11" -theme "WhiteSur-dark" &
 blueberry-tray &
 clipmenud &
 caffeine -a &
