5bb8a3dd6c63d239112cd2a9e8dd3ca90670fddd
wochap <gean.marroquin@gmail.com>
2022-01-05 21:03:41 -0500

    refactor: lorri

--- config/macos.nix
+++ config/macos.nix
@@ -7,6 +7,7 @@
     ./users/user-darwin.nix
     ./mixins/fonts.nix
     ./mixins/overlays.nix
+    ./mixins/lorri.nix
   ];
 
   config = {
@@ -21,24 +22,6 @@
     environment.variables = {
       # TERM = "xterm-kitty";
       TERMINFO_DIRS = "${pkgs.kitty.terminfo.outPath}/share/terminfo:$TERMINFO_DIRS";
-    };
-
-    # XXX: Copied verbatim from https://github.com/iknow/nix-channel/blob/7bf3584e0bef531836050b60a9bbd29024a1af81/darwin-modules/lorri.nix
-    launchd.user.agents = {
-      "lorri" = {
-        serviceConfig = {
-          WorkingDirectory = (builtins.getEnv "HOME");
-          EnvironmentVariables = {};
-          KeepAlive = true;
-          RunAtLoad = true;
-          StandardOutPath = "/var/tmp/lorri.log";
-          StandardErrorPath = "/var/tmp/lorri.log";
-        };
-        script = ''
-          source ${config.system.build.setEnvironment}
-          exec ${pkgs.lorri}/bin/lorri daemon
-        '';
-      };
     };
 
     environment = {
