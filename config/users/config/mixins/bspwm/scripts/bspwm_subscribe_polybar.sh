305a24edb511b4a79b3380d52762a1f34ba58c31
wochap <gean.marroquin@gmail.com>
2022-01-07 20:12:08 -0500

    feat: add hidden windows indicator

--- config/users/config/mixins/bspwm/scripts/bspwm_subscribe_polybar.sh
+++ config/users/config/mixins/bspwm/scripts/bspwm_subscribe_polybar.sh
@@ -2,7 +2,20 @@
 
 # For custom Polybar module `bspwm_monocle_nb_windows`
 _trigger_polybar_windows_number() {
-  desktop_layout=$(bspc query -T -d|jq -r .layout)
+  hidden_windows=$(bspc query -N -n .hidden.local.window | wc -l)
+  [[ $hidden_windows -gt "0" ]] && {
+    echo $hidden_windows
+    echo "show"
+    (polybar-msg hook bspwm_hidden_windows 1) &>/dev/null
+    (polybar-msg hook bspwm_hidden_windows_sep 1) &>/dev/null
+  } || {
+    echo $hidden_windows
+    echo "hide"
+    (polybar-msg hook bspwm_hidden_windows 2) &>/dev/null
+    (polybar-msg hook bspwm_hidden_windows_sep 2) &>/dev/null
+  }
+
+  desktop_layout=$(bspc query -T -d | jq -r .layout)
   [[ $desktop_layout == "monocle" ]] && {
     (polybar-msg hook bspwm_monocle_nb_windows 1) &>/dev/null
     (polybar-msg hook bspwm_monocle_nb_windows_sep 1) &>/dev/null
@@ -13,22 +26,11 @@ _trigger_polybar_windows_number() {
     # TODO: show polybar?
   }
 }
+
 while read -a line; do
-	case "$line" in
-    desktop_focus)
-      _trigger_polybar_windows_number
-      ;;
-    desktop_layout)
-      _trigger_polybar_windows_number
-      ;;
-    node_add)
-      _trigger_polybar_windows_number
-      ;;
-    node_stack)
-      _trigger_polybar_windows_number
-      ;;
-    node_remove)
-      _trigger_polybar_windows_number
-      ;;
-	esac
-done < <(bspc subscribe desktop_layout desktop_focus node_add node_remove node_stack node_focus)
+  case "$line" in
+  *)
+    _trigger_polybar_windows_number
+    ;;
+  esac
+done < <(bspc subscribe desktop_layout desktop_focus node_add node_remove node_stack node_focus node_flag)
