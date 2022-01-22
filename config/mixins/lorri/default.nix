240f01b021059022018c147b5bd0cb2f72b69605
wochap <gean.marroquin@gmail.com>
2022-01-22 16:52:10 -0500

    fix: direnv

--- config/mixins/lorri/default.nix
+++ config/mixins/lorri/default.nix
@@ -6,11 +6,23 @@ let
   mkOutOfStoreSymlink = hmConfig.lib.file.mkOutOfStoreSymlink;
   configDirectory = config._configDirectory;
   currentDirectory = "${configDirectory}/config/mixins/lorri";
+  # newer versions of direnv, auto load .env files
+  customDirenv = pkgs.direnv.overrideAttrs(o: {
+    src = pkgs.fetchFromGitHub {
+      repo = "direnv";
+      owner = "direnv";
+      rev = "3c84231d9781ef1dd3df81a7b2de52e1dad58f62";
+      sha256 = "sha256-dokZDGs09Q/P3QN3gfnUB25vim/+VN5xxKt9FkFfTb8=";
+    };
+    # temporarily disable tests, check if they can be reenabled with the next release
+    doCheck = true;
+  });
+
 in {
   config = {
     environment.systemPackages = with pkgs;
       [
-        direnv # auto run nix-shell
+        customDirenv # auto run nix-shell
       ];
 
     services.lorri.enable = true;
