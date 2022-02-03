2db4f6a51430363a7798b02fe7e976de09075bd9
wochap <gean.marroquin@gmail.com>
2022-02-03 16:28:46 -0500

    rice(wofi)

--- config/users/mixins/wofi/scripts/wofi-clipboard.sh
+++ config/users/mixins/wofi/scripts/wofi-clipboard.sh
@@ -8,6 +8,6 @@ monitor_size=$(swaymsg -t get_outputs | jq '.[] | select(.focused) | .rect.width
 monitor_width=$(printf "$monitor_size" | sed -n '1p')
 monitor_height=$(printf "$monitor_size" | sed -n '2p')
 xoffset=$(echo "$monitor_width/2-$wofi_real_width/2" | bc)
-yoffset="300"
+yoffset=$(echo "$monitor_height*0.2" | bc)
 
 clipman pick --tool="wofi" --tool-args="--width $wofi_width --lines 15 --location top_left --yoffset $yoffset --xoffset $xoffset"
