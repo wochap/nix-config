e38224e71a1fd2511093be0d5b08aacb16e2aaa4
wochap <gean.marroquin@gmail.com>
2022-02-12 07:58:32 -0500

    fix: fzf history

--- config/users/mixins/zsh/default.nix
+++ config/users/mixins/zsh/default.nix
@@ -1,28 +1,28 @@
-{ config, pkgs, lib, inputs,  ... }:
+{ config, pkgs, lib, inputs, ... }:
 
 let
   isDarwin = config._displayServer == "darwin";
   userName = config._userName;
-in
-{
+  hmConfig = config.home-manager.users.${userName};
+in {
   config = {
     environment = {
-      systemPackages = with pkgs; [
-        exa
-      ];
-      pathsToLink = [
-        "/share/zsh"
-      ];
+      systemPackages = with pkgs; [ exa ];
+      pathsToLink = [ "/share/zsh" ];
     };
+
     home-manager.users.${userName} = {
+      # Make sure we create a cache directory since some plugins expect it to exist
+      # See: https://github.com/nix-community/home-manager/issues/761
+      home.file."${hmConfig.xdg.cacheHome}/oh-my-zsh/.keep".text = "";
+
       programs.zsh = {
         enable = true;
         dotDir = ".config/zsh";
         initExtra = ''
+          # oh-my-zsh
           export ZSH="${inputs.ohmyzsh}"
           ${builtins.readFile ./ohmyzsh.zsh}
-          ${builtins.readFile ./zshrc.zsh}
-
           source ${inputs.ohmyzsh}/lib/key-bindings.zsh
           source ${inputs.ohmyzsh}/lib/completion.zsh
           source ${inputs.ohmyzsh}/lib/clipboard.zsh
@@ -31,6 +31,12 @@ in
           source ${inputs.ohmyzsh}/plugins/copyfile/copyfile.plugin.zsh
           source ${inputs.ohmyzsh}/plugins/dirhistory/dirhistory.plugin.zsh
 
+          if [[ $options[zle] = on ]]; then
+            source ${pkgs.fzf}/share/fzf/completion.zsh
+            source ${pkgs.fzf}/share/fzf/key-bindings.zsh
+          fi
+
+          ${builtins.readFile ./zshrc.zsh}
           ${builtins.readFile ./awesome.zsh}
 
           source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
@@ -48,12 +54,8 @@ in
           # Shares current history file between all sessions as soon as shell closes
           share = true;
         };
-        prezto = {
-          enable = false;
-        };
-        oh-my-zsh = {
-          enable = false;
-        };
+        prezto = { enable = false; };
+        oh-my-zsh = { enable = false; };
         shellAliases = lib.mkMerge [
           config.environment.shellAliases
           {
@@ -61,7 +63,8 @@ in
 
             # Setup exa
             ls = lib.mkForce "exa --icons --group-directories-first --across";
-            la = lib.mkForce "exa --icons --group-directories-first --all --long";
+            la =
+              lib.mkForce "exa --icons --group-directories-first --all --long";
 
             # Setup ptSh
             pwdd = "ptpwd";
@@ -73,23 +76,9 @@ in
         ];
       };
 
-      programs.starship = {
-        enable = true;
-        enableZshIntegration = true;
-        settings = {
-          add_newline = false;
-          character = {
-            success_symbol = "[➜](bold green)";
-            error_symbol = "[➜](bold red)";
-          };
-          nix_shell = {
-            disabled = true;
-          };
-          package = {
-            disabled = true;
-          };
-        };
-      };
+      programs.starship.enableZshIntegration = true;
+
+      # programs.fzf.enableZshIntegration = true;
     };
   };
 }
