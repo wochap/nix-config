6f09f3112a7a08f632a74d1db837c64b344838db
wochap <gean.marroquin@gmail.com>
2022-02-20 18:51:37 -0500

    stype

--- config/users/mixins/vim/default.nix
+++ config/users/mixins/vim/default.nix
@@ -3,75 +3,73 @@
 let
   isDarwin = config._displayServer == "darwin";
   userName = config._userName;
-  localPkgs = import ../../../packages { pkgs = pkgs; lib = lib; };
+  localPkgs = import ../../../packages {
+    pkgs = pkgs;
+    lib = lib;
+  };
   # extraPackages = with pkgs; [
   #   localPkgs.customNodePackages.typescript
   #   localPkgs.customNodePackages.typescript-language-server
   # ];
   # extraMakeWrapperArgs = ''--suffix PATH : "${lib.makeBinPath extraPackages}"'';
-in
-{
+in {
   config = {
     environment = {
-      variables = {
-        EDITOR =  "nvim";
-      };
+      variables = { EDITOR = "nvim"; };
 
-      systemPackages = with pkgs; [
-        # oni2
-        # neovim-qt # better fractional scaling support
-        # uivonim
+      systemPackages = with pkgs;
+        [
+          # oni2
+          # neovim-qt # better fractional scaling support
+          # uivonim
 
-        # neovim # TODO: passextraMakeWrapperArgs?
-        neovim-nightly
+          # neovim # TODO: passextraMakeWrapperArgs?
+          neovim-nightly
 
-        # required by treesitter
-        tree-sitter
+          # required by treesitter
+          tree-sitter
 
-        # required by telescope
-        ripgrep
-        fd
+          # required by telescope
+          ripgrep
+          fd
 
-        # required by null-ls
-        localPkgs.customNodePackages."@fsouza/prettierd"
-        localPkgs.customNodePackages.markdownlint
-        localPkgs.customNodePackages.stylelint
-        lua51Packages.luacheck
-        nixfmt
-        nodePackages.eslint_d
-        python39Packages.pylint
-        shellcheck
-        shfmt
-        statix
-        stylua
+          # required by null-ls
+          localPkgs.customNodePackages."@fsouza/prettierd"
+          localPkgs.customNodePackages.markdownlint
+          localPkgs.customNodePackages.stylelint
+          lua51Packages.luacheck
+          nixfmt
+          nodePackages.eslint_d
+          python39Packages.pylint
+          shellcheck
+          shfmt
+          statix
+          stylua
 
-        # required by lspconfig
-        clang-tools
-        flow
-        localPkgs.customNodePackages."@tailwindcss/language-server"
-        localPkgs.customNodePackages.cssmodules-language-server
-        localPkgs.customNodePackages.emmet-ls
-        nodePackages.bash-language-server
-        nodePackages.svelte-language-server
-        nodePackages.vscode-langservers-extracted
-        rnix-lsp
-        # localPkgs.customNodePackages.typescript
-        # localPkgs.customNodePackages.typescript-language-server
-        # npm i typescript typescript-language-server -g
-      ] ++ (if (!isDarwin) then [
-        neovide
+          # required by lspconfig
+          clang-tools
+          flow
+          localPkgs.customNodePackages."@tailwindcss/language-server"
+          localPkgs.customNodePackages.cssmodules-language-server
+          localPkgs.customNodePackages.emmet-ls
+          nodePackages.bash-language-server
+          nodePackages.svelte-language-server
+          nodePackages.vscode-langservers-extracted
+          rnix-lsp
+          # localPkgs.customNodePackages.typescript
+          # localPkgs.customNodePackages.typescript-language-server
+          # npm i typescript typescript-language-server -g
+        ] ++ (if (!isDarwin) then [
+          neovide
 
-        sumneko-lua-language-server
-      ] else []);
-      shellAliases = {
-        nv = "nvim";
-      };
+          sumneko-lua-language-server
+        ] else
+          [ ]);
+      shellAliases = { nv = "nvim"; };
     };
 
     home-manager.users.${userName} = {
-      home.file = {
-        ".vimrc".source = ./dotfiles/.vimrc;
-      };
+      home.file = { ".vimrc".source = ./dotfiles/.vimrc; };
 
       programs.vim = {
         enable = true;
