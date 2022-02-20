2278fc63eb67ef8e69376d75d4e3831a81016c76
wochap <gean.marroquin@gmail.com>
2022-02-20 18:51:53 -0500

    style

--- config/mixins/gnome-pkgs.nix
+++ config/mixins/gnome-pkgs.nix
@@ -4,24 +4,26 @@
   config = {
     environment = {
       systemPackages = with pkgs; [
-        # APPS
-        inkscape # photo editor cli/gui
+        blanket
         deluge # torrent client
+        inkscape # photo editor cli/gui
+        networkmanagerapplet
+
+        # APPS
         gnome.cheese # test webcam
         gnome.dconf-editor
         gnome.eog # image viewer
-        # gnome.geary # email client
         gnome.gnome-calculator
         gnome.gnome-calendar
         gnome.gnome-clocks
-        gnome.gnome-control-center # add google account for gnome apps
         gnome.gnome-font-viewer
         gnome.gnome-sound-recorder # test microphone
         gnome.gnome-system-monitor
         gnome.gnome-todo
         gnome.nautilus # file manager
         gnome.pomodoro
-        networkmanagerapplet
+        # gnome.geary # email client
+        # gnome.gnome-control-center # add google account for gnome apps
       ];
     };
   };
