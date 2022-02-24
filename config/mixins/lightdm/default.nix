f22692b8c289364363c5140f44ecefd5a4310085
wochap <gean.marroquin@gmail.com>
2022-02-24 07:32:50 -0500

    refactor

--- config/mixins/lightdm/default.nix
+++ config/mixins/lightdm/default.nix
@@ -1,8 +1,6 @@
 { config, pkgs, lib, inputs, ... }:
 
-let
-  isHidpi = config._isHidpi;
-  globals = import ../globals.nix { inherit config pkgs lib inputs; };
+let globals = import ../globals.nix { inherit config pkgs lib inputs; };
 in {
   config = {
     services.xserver.displayManager = {
