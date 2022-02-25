0554d020140888800ea1ace8a0ccb85376e7813c
wochap <gean.marroquin@gmail.com>
2022-02-24 21:51:31 -0500

    fix: sticky windows
    make pomodoro gtk info box sticky and floa

--- config/users/mixins/bspwm/scripts/bspwm_external_rules.sh
+++ config/users/mixins/bspwm/scripts/bspwm_external_rules.sh
@@ -9,24 +9,36 @@ instance=$(echo "${3}" | xargs)
 # echo "$wid $class $instance" > /tmp/bspc-external-rules
 
 case "$class" in
-  .eww-border )
-    echo layer=below;;
-  Nitrogen | zoom | Org.gnome.clocks | Gnome-pomodoro | Gnome-todo )
-    case "$(xprop -id "$wid" _NET_WM_WINDOW_TYPE)" in
-      *_NET_WM_WINDOW_TYPE_NORMAL* )
-        echo state=pseudo_tiled;;
-    esac;;
-  Firefox )
-    case "$(xprop -id "$wid" WM_WINDOW_ROLE)" in
-      *PictureInPicture* )
-        echo state=floating sticky=on;;
-      *About* )
-        echo state=floating center=true;;
-    esac;;
+.eww-border)
+  echo layer=below
+  ;;
+Nitrogen | zoom | Org.gnome.clocks | Gnome-pomodoro | Gnome-todo)
+  case "$(xprop -id "$wid" _NET_WM_WINDOW_TYPE)" in
+  *_NET_WM_WINDOW_TYPE_NORMAL*)
+    echo state=pseudo_tiled
+    ;;
+  esac
+  ;;
+Firefox)
+  case "$(xprop -id "$wid" WM_WINDOW_ROLE)" in
+  *PictureInPicture*)
+    echo state=floating sticky=on
+    ;;
+  *About*)
+    echo state=floating center=true
+    ;;
+  esac
+  ;;
 esac
 
 case "$(xprop -id "$wid" _NET_WM_WINDOW_TYPE)" in
-  *_NET_WM_WINDOW_TYPE_DIALOG* )
-    echo state=floating center=true;;
+*_NET_WM_WINDOW_TYPE_DIALOG*)
+  echo state=floating center=true
+  ;;
 esac
 
+case "$(xprop -id "$wid" _NET_WM_STATE)" in
+*_NET_WM_STATE_STICKY*)
+  echo state=floating center=true sticky=on
+  ;;
+esac
