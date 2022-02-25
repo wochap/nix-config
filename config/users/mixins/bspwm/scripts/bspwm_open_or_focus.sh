7fd5ab14f8c6af37d35bde3abbe0be3afca448cc
wochap <gean.marroquin@gmail.com>
2022-02-25 07:57:41 -0500

    fix bspwm_open_or_focus scripts

--- config/users/mixins/bspwm/scripts/bspwm_open_or_focus.sh
+++ config/users/mixins/bspwm/scripts/bspwm_open_or_focus.sh
@@ -1,22 +1,12 @@
 #!/usr/bin/env bash
 
-# Get id of process by class name and then fallback to instance name
-id=$(xdo id -N "${1}" || xdo id -n "${1}")
+class="$1"
+runstring="$2"
 
-executable=${1}
-shift
-
-while [ -n "${1}" ]; do
-  case ${1} in
-  *)
-    id=$(head -1 <<<"${id}" | cut -f1 -d' ')
-    ;;
-  esac
-  shift
-done
+id=$(xdo id -N "$class")
 
 if [ -z "${id}" ]; then
-  coproc $executable
+  exec "$runstring" >/dev/null 2>&1 &
 else
   while read -r instance; do
     bspc node "${instance}" --focus
