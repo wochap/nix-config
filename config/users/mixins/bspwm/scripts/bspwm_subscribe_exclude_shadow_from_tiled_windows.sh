5017785d5a4a50a33b96cbfe7a40c56a390b417c
wochap <gean.marroquin@gmail.com>
2022-02-27 17:11:06 -0500

    rice: hide shadow on tiled window

--- config/users/mixins/bspwm/scripts/bspwm_subscribe_exclude_shadow_from_tiled_windows.sh
+++ config/users/mixins/bspwm/scripts/bspwm_subscribe_exclude_shadow_from_tiled_windows.sh
@@ -1,23 +1,11 @@
+#!/usr/bin/env sh
+
 bspc subscribe node_state | while read -r _ _ _ node state status; do
-  if [[ "$state" == "floating" ]]; then
+  if [[ "$state" == "tiled" ]]; then
     case "$status" in
-      off) xprop -id "$node" -remove _PICOM_SHADOW;;
-      on) xprop -id "$node" -f _PICOM_SHADOW 32c -set _PICOM_SHADOW 1;;
+      off) xprop -id "$node" -remove _BSPWM_NODE_TILED;;
+      on) xprop -id "$node" -f _BSPWM_NODE_TILED 32c -set _BSPWM_NODE_TILED 1;;
     esac
   fi
 done
 
-# bspc subscribe node_focus | while read -r _ _ _ focused_node; do
-#   floating_nodes=$(bspc query --nodes --node .floating)
-#   nodes=$(bspc query --nodes)
-#   for node in $nodes
-#   do
-#     is_node_floating=$([[ " ${floating_nodes[@]} " =~ " ${node} " ]] && echo "true" || echo "false")
-#     if [[ $is_node_floating == "true" || $node == $focused_node  ]]
-#     then
-#       xprop -id "$node" -f _PICOM_SHADOW 32c -set _PICOM_SHADOW 1
-#     else
-#       xprop -id "$node" -remove _PICOM_SHADOW
-#     fi
-#   done
-# done
