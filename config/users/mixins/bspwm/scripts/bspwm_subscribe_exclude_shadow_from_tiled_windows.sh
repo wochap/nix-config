d7dfab8377c798ac47030a99d28f833a49daa0ea
wochap <gean.marroquin@gmail.com>
2022-02-28 18:35:26 -0500

    fix

--- config/users/mixins/bspwm/scripts/bspwm_subscribe_exclude_shadow_from_tiled_windows.sh
+++ config/users/mixins/bspwm/scripts/bspwm_subscribe_exclude_shadow_from_tiled_windows.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env sh
 
-bspc subscribe node_state | while read -r _ _ _ node state status; do
-  if [[ "$state" == "tiled" ]]; then
-    case "$status" in
-      off) xprop -id "$node" -remove _BSPWM_NODE_TILED;;
-      on) xprop -id "$node" -f _BSPWM_NODE_TILED 32c -set _BSPWM_NODE_TILED 1;;
-    esac
+bspc subscribe node_state node_add | while read -r type _ _ node state status; do
+  if [[ $type == "node_state" ]]; then
+    if [[ "$state" == "tiled" ]]; then
+      case "$status" in
+      off) xprop -id "$node" -remove _BSPWM_NODE_TILED ;;
+      on) xprop -id "$node" -f _BSPWM_NODE_TILED 32c -set _BSPWM_NODE_TILED 1 ;;
+      esac
+    fi
+  else
+    node="$state"
+    tiled_nodes=($(bspc query --nodes --node .window.local.tiled))
+    if [[ " ${tiled_nodes[*]} " =~ " ${node} " ]]; then
+      xprop -id "$node" -f _BSPWM_NODE_TILED 32c -set _BSPWM_NODE_TILED 1
+    fi
   fi
 done
-
