87e65984ee2838bff10b47e14051115a23ac6786
wochap <gean.marroquin@gmail.com>
2022-03-08 18:50:09 -0500

    style

--- config/users/mixins/android.nix
+++ config/users/mixins/android.nix
@@ -1,36 +1,35 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, inputs, ... }:
 
 let
   userName = config._userName;
   phoneId = "04e8";
   android-repo = builtins.fetchTarball {
-    url = https://github.com/tadfisher/android-nixpkgs/archive/14306de794ed518c548d20c9c16dbe12a305b9e6.tar.gz;
+    url =
+      "https://github.com/tadfisher/android-nixpkgs/archive/14306de794ed518c548d20c9c16dbe12a305b9e6.tar.gz";
     sha256 = "10rvcm8yhz0c4y5hr3di5i5bbhjclb63r77sm1bdhg67ri9zzj4j";
   };
-  android-pkgs = (import "${android-repo}/default.nix") {
-    channel = "stable";
-  };
+  android-pkgs = (import "${android-repo}/default.nix") { channel = "stable"; };
   android-hm = (import "${android-repo}/hm-module.nix");
-  finalPackage = android-pkgs.sdk (sdk: with sdk; [
-    # Required by react-native/flutter
-    build-tools-29-0-2
-    build-tools-30-0-3
-    cmdline-tools-latest
-    emulator
-    platform-tools
-    platforms-android-29
+  finalPackage = android-pkgs.sdk (sdk:
+    with sdk; [
+      # Required by react-native/flutter
+      build-tools-29-0-2
+      build-tools-30-0-3
+      cmdline-tools-latest
+      emulator
+      platform-tools
+      platforms-android-29
 
-    # Required to create emulator
-    build-tools-31-0-0
-    platforms-android-30
-    platforms-android-31
-    sources-android-30
-    system-images-android-30-google-apis-playstore-x86
-    system-images-android-30-google-apis-x86
-  ]);
+      # Required to create emulator
+      build-tools-31-0-0
+      platforms-android-30
+      platforms-android-31
+      sources-android-30
+      system-images-android-30-google-apis-playstore-x86
+      system-images-android-30-google-apis-x86
+    ]);
   path = "Android/Sdk";
-in
-{
+in {
   config = {
     programs.adb.enable = true;
 
@@ -39,24 +38,19 @@ in
     '';
 
     environment = {
-      systemPackages = with pkgs; [
-        jdk8
-        jdk11
-        android-studio
-      ];
+      systemPackages = with pkgs; [ jdk8 jdk11 android-studio ];
       sessionVariables = {
         # TODO: use android-studio jre path?
         JAVA_HOME = pkgs.jdk11.home;
 
         # Fix react native aapt2 errors
-        GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${finalPackage}/share/android-sdk/build-tools/30.0.3/aapt2";
+        GRADLE_OPTS =
+          "-Dorg.gradle.project.android.aapt2FromMavenOverride=${finalPackage}/share/android-sdk/build-tools/30.0.3/aapt2";
       };
     };
 
     home-manager.users.${userName} = {
-      imports = [
-        android-hm
-      ];
+      imports = [ android-hm ];
 
       # TODO: use android hm options
       # Fine, I'll do it myself
