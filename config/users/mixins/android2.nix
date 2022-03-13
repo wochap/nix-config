2cbb2d709bb44784a53cb932f62774f38ce64463
wochap <gean.marroquin@gmail.com>
2022-03-13 10:14:19 -0500

    saving changes

--- config/users/mixins/android2.nix
+++ config/users/mixins/android2.nix
@@ -4,71 +4,64 @@ let
   userName = config._userName;
   hmConfig = config.home-manager.users.${userName};
 
+  android-sdk-home-path = "Android/Sdk";
   phoneId = "04e8";
-  path = "Android/Sdk";
 
   android-studio-stable = pkgs.androidStudioPackages.stable;
   android-sdk = inputs.android-nixpkgs.sdk.${system} (sdkPkgs:
     with sdkPkgs; [
-      # Required by React Native
-      build-tools-29-0-3
-      build-tools-30-0-3
       cmdline-tools-latest
       emulator
       platform-tools
-      platforms-android-29
+
+      # Android 30
+      build-tools-30-0-2
       platforms-android-30
 
       # Required by android emulator
       sources-android-30
-      system-images-android-30-google-apis-playstore-x86-64
-      system-images-android-30-google-apis-x86-64
+      system-images-android-30-google-apis-playstore-x86
+      system-images-android-30-google-apis-x86
+
+      # Android 29
+      # build-tools-29-0-3
+      # platforms-android-29
+      # sources-android-29
+      # system-images-android-29-google-apis-playstore-x86-64
+      # system-images-android-29-google-apis-x86-64
     ]);
 in {
   config = {
-    # nixpkgs.overlays = [ inputs.android-nixpkgs.overlay ];
-
+    # Enable android device debugging
     programs.adb.enable = true;
-
     services.udev.extraRules = ''
       SUBSYSTEM=="usb", ATTR{idVendor}=="${phoneId}", MODE="0666", GROUP="plugdev"
     '';
 
-    environment = {
-      systemPackages = with pkgs; [
-        jdk11
-        gradle
-        android-sdk
-        android-studio-stable
-      ];
-      variables = { _JAVA_AWT_WM_NONREPARENTING = "1"; };
-      sessionVariables = {
-        JAVA_HOME = pkgs.jdk11.home;
-        ANDROID_HOME = "${android-sdk}/share/android-sdk";
-        ANDROID_SDK_ROOT = "${android-sdk}/share/android-sdk";
-
-        # Fix react native aapt2 errors
-        GRADLE_OPTS =
-          "-Dorg.gradle.project.android.aapt2FromMavenOverride=${android-sdk}/share/android-sdk/build-tools/30.0.3/aapt2";
-      };
-    };
-
     home-manager.users.${userName} = {
-      # imports = [ android-hm ];
-      # imports = [ inputs.android-nixpkgs.hmModule ];
-
       config = {
-        # TODO: use android hm options?
-        # android-sdk.enable = false;
-
-        # Fine, I'll do it myself
         home = {
-          # file.${path}.source = "${androidSdkPkg}/share/android-sdk";
-          # packages = [ androidSdkPkg ];
+          file.${android-sdk-home-path}.source =
+            "${android-sdk}/share/android-sdk";
+
+          packages = with pkgs; [
+            android-sdk
+            android-studio-stable
+            gradle
+            jdk11
+          ];
 
           sessionVariables = {
-            # ANDROID_HOME = "/home/${userName}/${path}";
-            # ANDROID_SDK_ROOT = "/home/${userName}/${path}";
+            # Required by android-studio on wm
+            _JAVA_AWT_WM_NONREPARENTING = "1";
+
+            JAVA_HOME = pkgs.jdk11.home;
+            ANDROID_HOME = "/home/${userName}/${android-sdk-home-path}";
+            ANDROID_SDK_ROOT = "/home/${userName}/${android-sdk-home-path}";
+
+            # Fix react-native aapt2 errors
+            GRADLE_OPTS =
+              "-Dorg.gradle.project.android.aapt2FromMavenOverride=${android-sdk}/share/android-sdk/build-tools/30.0.2/aapt2";
           };
         };
 
