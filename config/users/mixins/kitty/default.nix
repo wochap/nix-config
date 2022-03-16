d12dcc9fb33b1eab3de29e47db0ade2ab9d83c3a
wochap <gean.marroquin@gmail.com>
2022-03-16 08:00:55 -0500

    rice

--- config/users/mixins/kitty/default.nix
+++ config/users/mixins/kitty/default.nix
@@ -118,6 +118,22 @@ in {
           # };
           nix_shell = { disabled = true; };
           package = { disabled = true; };
+
+          # Dracula theme
+          aws.style = "bold #ffb86c";
+          cmd_duration.style = "bold #f1fa8c";
+          directory.style = "bold #50fa7b";
+          hostname.style = "bold #ff5555";
+          git_branch.style = "bold #ff79c6";
+          git_status.style = "bold #ff5555";
+          username = {
+            format = "[$user]($style) on ";
+            style_user = "bold #bd93f9";
+          };
+          character = {
+            success_symbol = "[❯](bold #f8f8f2)";
+            error_symbol = "[❯](bold #ff5555)";
+          };
         };
       };
     };
