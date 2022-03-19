bee2b7ebb2420ee49666d7dec5981975ad9d2ba6
wochap <gean.marroquin@gmail.com>
2022-03-18 19:22:05 -0500

    feat: update flake.lock

--- config/mixins/nix-common.nix
+++ config/mixins/nix-common.nix
@@ -60,11 +60,13 @@ in {
       binaryCachePublicKeys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "colemickens.cachix.org-1:bNrJ6FfMREB4bd4BOjEN85Niu8VcPdQe4F4KxVsb/I4="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
       ];
       binaryCaches = [
         "https://cache.nixos.org"
         "https://colemickens.cachix.org"
+        "https://nix-community.cachix.org"
         "https://nixpkgs-wayland.cachix.org"
       ];
       trustedUsers = [ "@wheel" "root" ];
