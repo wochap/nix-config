a9585434fb5981379aa41041f4d348758ff812b5
wochap <gean.marroquin@gmail.com>
2022-03-30 12:23:16 -0500

    macos

--- config/macos.nix
+++ config/macos.nix
@@ -23,7 +23,7 @@ in {
     # sudo yabai --load-sa
     services.yabai = {
       enable = true;
-      enableScriptingAddition = true;
+      # enableScriptingAddition = true;
       package = pkgs.yabai;
       config = {
         # layout
@@ -31,12 +31,13 @@ in {
         auto_balance = "on";
         split_ratio = "0.50";
         window_placement = "second_child";
+        window_topmost = "on";
         # Gaps
-        window_gap = 18;
-        top_padding = 46;
-        bottom_padding = 18;
-        left_padding = 18;
-        right_padding = 18;
+        window_gap = 0;
+        top_padding = 0;
+        bottom_padding = 0;
+        left_padding = 0;
+        right_padding = 0;
         # shadows and borders
         window_shadow = "on";
         window_border = "off";
@@ -46,21 +47,28 @@ in {
         active_window_opacity = "1.0";
         normal_window_opacity = "1.0";
         # mouse
-        mouse_modifier = "cmd";
+        focus_follows_mouse = "autofocus";
+        mouse_modifier = "alt";
         mouse_action1 = "move";
         mouse_action2 = "resize";
         mouse_drop_action = "swap";
       };
       extraConfig = ''
+        # necessary to load scripting-addition during startup on macOS Big Sur
+        # *yabai --load-sa* is configured to run through sudo without a password
+        sudo yabai --load-sa
+        yabai -m signal --add event=dock_did_restart action="sudo yabai --load-sa"
+
         # rules
-        yabai -m rule --add app='Firefox' manage=on
         yabai -m rule --add app='System Preferences' manage=off
         yabai -m rule --add app='Activity Monitor' manage=off
+        yabai -m rule --add app='Notes' manage=off
+        yabai -m rule --add app='Simulator' manage=off
       '';
     };
 
     services.spacebar = {
-      enable = true;
+      enable = false;
       package = pkgs.spacebar;
       config = {
         position = "top";
@@ -94,31 +102,65 @@ in {
       enable = true;
       skhdConfig = builtins.readFile ./users/dotfiles-darwin/.skhdrc;
     };
+
+    homebrew = {
+      enable = false;
+      autoUpdate = true;
+      cleanup = "zap"; # uninstall
+      global = {
+        brewfile = true;
+        noLock = true;
+      };
+
+      taps = [
+        "homebrew/bundle"
+        "homebrew/cask"
+        "homebrew/cask-fonts"
+        "homebrew/cask-versions"
+        "homebrew/core"
+        "homebrew/services"
+        "mongodb/brew"
+      ];
+
+      casks = [
+        "robo-3t"
+        "visual-studio-code"
+        "iterm2"
+        "firefox"
+        "google-chrome"
+        "vlc"
+        "postman"
+        "insomnia"
+        "zoom"
+        "keka"
+        "figma"
+        "alt-tab"
+        "finicky"
+        "bitwarden"
+        "smcfancontrol"
+        # "obs"
+        # "streamlabs-obs" # OBS with audio?
+      ];
+
+      brews = [
+        "clipcopy"
+        "mongodb-database-tools"
+        "mongodb-community@5.0"
+        "lua-language-server"
+        "mas"
+        "jq"
+      ];
+
+      masApps = {
+        "Slack" = 803453959 ;
+        "CopyClip - Clipboard History" = 595191960;
+      };
+
+      extraConfig = ''
+        cask_args appdir: "/Applications"
+      '';
+    };
   };
 
-  # homebrew = {
-  #   brewPrefix = "/opt/homebrew/bin";
-  #   enable = true;
-  #   autoUpdate = true;
-  #   cleanup = "zap"; # keep it clean
-  #   global = {
-  #     brewfile = true;
-  #     noLock = true;
-  #   };
-  #
-  #   taps = [
-  #     "homebrew/core" # core
-  #     "homebrew/cask" # we're using this for casks, after all
-  #     "homebrew/cask-versions" # needed for firefox-nightly and discord-canary
-  #   ];
-  #
-  #   casks = [
-  #     "raycast" # Launcher
-  #     "prusaslicer" # 3d printing
-  #     "discord-canary" # discord for macOS
-  #     "firefox-nightly" # my browser of choice
-  #     "nvidia-geforce-now" # game streaming
-  #   ];
-  # };
 
 }
