511c2fd71efe93869906cb3373f2b650bbecb1ef
wochap <gean.marroquin@gmail.com>
2022-05-10 10:40:03 -0500

    refactor

--- config/mixins/nix-common.nix
+++ config/mixins/nix-common.nix
@@ -3,53 +3,6 @@
 
 let theme = config._theme;
 in {
-  # https://discourse.nixos.org/t/using-mkif-with-nested-if/5221/4
-  # https://discourse.nixos.org/t/best-resources-for-learning-about-the-nixos-module-system/1177/4
-  # https://nixos.org/manual/nixos/stable/index.html#sec-option-types
-  options = {
-    _displayServer = lib.mkOption {
-      type = lib.types.str;
-      default = "";
-      example = "xorg"; # xorg, wayland, darwin
-      description = "Display server type, used by common config files.";
-    };
-    _isHidpi = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      example = true;
-      description = "Flag for hidpi displays.";
-    };
-    _userName = lib.mkOption {
-      type = lib.types.str;
-      default = "gean";
-      example = "gean";
-      description = "Default user name";
-    };
-    _homeDirectory = lib.mkOption {
-      type = lib.types.str;
-      default = "/home/gean";
-      example = "/home/gean";
-      description = "Path of user home folder";
-    };
-    _configDirectory = lib.mkOption {
-      type = lib.types.str;
-      default = "/home/gean/nix-config";
-      example = "/home/gean/nix-config";
-      description = "Path of config folder";
-    };
-    _theme = lib.mkOption {
-      type = lib.types.attrsOf (lib.types.nullOr lib.types.str);
-      default = { };
-      example = "{}";
-      description = "Theme colors";
-    };
-    _isNvidia = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      example = true;
-      description = "Flag for devices with nvidia.";
-    };
-  };
 
   config = {
     # Allow unfree packages
