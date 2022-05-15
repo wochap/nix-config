5d21b65dda790cf0908f539906cb796a360355c1
wochap <gean.marroquin@gmail.com>
2022-05-15 09:14:09 -0500

    fix

--- modules/bspwm/scripts/bspwm_autostart.sh
+++ modules/bspwm/scripts/bspwm_autostart.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 dex /run/current-system/sw/etc/xdg/autostart/xdg-user-dirs.desktop \
+  /run/current-system/sw/etc/xdg/autostart/tracker-miner-fs-3.desktop \
   /run/current-system/sw/etc/xdg/autostart/polkit-gnome-authentication-agent-1.desktop \
   /run/current-system/sw/etc/xdg/autostart/xfce4-power-manager.desktop &
   # /run/current-system/sw/etc/xdg/autostart/org.gnome.Evolution-alarm-notify.desktop \
