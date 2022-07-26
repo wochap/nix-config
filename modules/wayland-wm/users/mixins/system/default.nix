15f0d550496fcd0f8acfb78944e16827b37770c3
wochap <gean.marroquin@gmail.com>
2022-07-26 09:19:08 -0500

    refactor

---
+++ modules/wayland-wm/users/mixins/system/default.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, lib, ... }:
+
+{
+  config = {
+    environment = {
+      etc = {
+        "scripts/system/sway-lock.sh" = {
+          source = ./scripts/sway-lock.sh;
+          mode = "0755";
+        };
+        "scripts/system/color-picker.sh" = {
+          source = ./scripts/color-picker.sh;
+          mode = "0755";
+        };
+        "scripts/system/takeshot.sh" = {
+          source = ./scripts/takeshot.sh;
+          mode = "0755";
+        };
+        "scripts/system/recorder.sh" = {
+          source = ./scripts/recorder.sh;
+          mode = "0755";
+        };
+      };
+    };
+  };
+}
+
