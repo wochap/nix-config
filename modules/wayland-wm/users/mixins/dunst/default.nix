a9713e346e4f120be774d198b9c8e654ee9bb44e
wochap <gean.marroquin@gmail.com>
2023-04-24 13:05:14 -0500

    dunst wayland

---
+++ modules/wayland-wm/users/mixins/dunst/default.nix
@@ -0,0 +1,73 @@
+{ config, pkgs, lib, ... }:
+
+let
+  cfg = config._custom.xorgWm;
+  userName = config._userName;
+  hmConfig = config.home-manager.users.${userName};
+  inherit (hmConfig.lib.file) mkOutOfStoreSymlink;
+  configDirectory = config._configDirectory;
+  currentDirectory = "${configDirectory}/modules/xorg-wm/users/mixins/dunst";
+in {
+  config = lib.mkIf cfg.enable {
+    environment = {
+      systemPackages = with pkgs; [ dunst libnotify gnome-icon-theme ];
+      etc = {
+        "assets/notification.flac" = {
+          source = ./assets/notification.flac;
+          mode = "0755";
+        };
+
+        "scripts/dunst-toggle-mode.sh" = {
+          source = ./scripts/dunst-toggle-mode.sh;
+          mode = "0755";
+        };
+        "scripts/dunst-start.sh" = {
+          source = ./scripts/dunst-start.sh;
+          mode = "0755";
+        };
+        "scripts/play_notification.sh" = {
+          text = ''
+            #! ${pkgs.bash}/bin/bash
+
+            declare -i last_called=0
+            declare -i throttle_by=1
+
+            @debounce() {
+              if [[ ! -f ./executing ]]
+              then
+                touch ./executing
+                "$@"
+                retVal=$?
+                {
+                  sleep $throttle_by
+                  if [[ -f ./on-finish ]]
+                  then
+                    "$@"
+                    rm -f ./on-finish
+                  fi
+                  rm -f ./executing
+                } &
+                return $retVal
+              elif [[ ! -f ./on-finish ]]
+              then
+                touch ./on-finish
+              fi
+            }
+
+            @debounce ${pkgs.pulseaudio}/bin/paplay /etc/assets/notification.flac
+
+            wait $(jobs -p)
+          '';
+          mode = "0755";
+        };
+      };
+    };
+
+    home-manager.users.${userName} = {
+      xdg.configFile = {
+        "dunst/dunstrc".source =
+          mkOutOfStoreSymlink "${currentDirectory}/dotfiles/dunstrc";
+      };
+    };
+  };
+}
---
+++ modules/wayland-wm/users/mixins/dunst/scripts/dunst-start.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env sh
+
+killall dunst
+killall .dunst-wrapped
+
+# get dpi
+DPI=$(xrdb -query | grep dpi | sed "s/Xft.dpi://" | xargs)
+
+case "$DPI" in
+192)
+  HEIGHT="300"
+  HORIZONTAL_PADDING="40"
+  PADDING="40"
+  WIDTH="700"
+  MAX_ICON_SIZE="120"
+  MIN_ICON_SIZE="120"
+  OFFSET=32
+  BORDER_WIDTH=4
+  ;;
+144)
+  HEIGHT="225"
+  HORIZONTAL_PADDING="30"
+  PADDING="30"
+  WIDTH="525"
+  MAX_ICON_SIZE="90"
+  MIN_ICON_SIZE="90"
+  OFFSET=24
+  BORDER_WIDTH=3
+  ;;
+*)
+  HEIGHT="150"
+  HORIZONTAL_PADDING="20"
+  PADDING="20"
+  WIDTH="350"
+  MAX_ICON_SIZE="60"
+  MIN_ICON_SIZE="60"
+  OFFSET=16
+  BORDER_WIDTH=2
+  ;;
+esac
+
+# update dust scale in place
+sed --follow-symlinks --in-place -E "/^height=/s/[0-9.]+/$HEIGHT/" "$HOME/.config/dunst/dunstrc"
+sed --follow-symlinks --in-place -E "/^horizontal_padding=/s/[0-9.]+/$HORIZONTAL_PADDING/" "$HOME/.config/dunst/dunstrc"
+sed --follow-symlinks --in-place -E "/^padding=/s/[0-9.]+/$PADDING/" "$HOME/.config/dunst/dunstrc"
+sed --follow-symlinks --in-place -E "/^width=/s/[0-9.]+/$WIDTH/" "$HOME/.config/dunst/dunstrc"
+sed --follow-symlinks --in-place -E "/^max_icon_size=/s/[0-9.]+/$MAX_ICON_SIZE/" "$HOME/.config/dunst/dunstrc"
+sed --follow-symlinks --in-place -E "/^min_icon_size=/s/[0-9.]+/$MIN_ICON_SIZE/" "$HOME/.config/dunst/dunstrc"
+sed --follow-symlinks --in-place -E "/^offset=/s/[0-9.]+/$OFFSET/g" "$HOME/.config/dunst/dunstrc"
+sed --follow-symlinks --in-place -E "/^frame_width=/s/[0-9.]+/$BORDER_WIDTH/" "$HOME/.config/dunst/dunstrc"
+sed --follow-symlinks --in-place -E "/^separator_height=/s/[0-9.]+/$BORDER_WIDTH/" "$HOME/.config/dunst/dunstrc"
+
+dunst &
