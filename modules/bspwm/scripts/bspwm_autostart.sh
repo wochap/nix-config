412d2fbf27699622856b7887d5bc69c339f7e3e9
wochap <gean.marroquin@gmail.com>
2023-06-12 22:42:53 -0500

    f

--- modules/bspwm/scripts/bspwm_autostart.sh
+++ modules/bspwm/scripts/bspwm_autostart.sh
@@ -4,11 +4,6 @@ dex /run/current-system/sw/etc/xdg/autostart/xdg-user-dirs.desktop \
   /run/current-system/sw/etc/xdg/autostart/tracker-miner-fs-3.desktop \
   /run/current-system/sw/etc/xdg/autostart/polkit-gnome-authentication-agent-1.desktop \
   /run/current-system/sw/etc/xdg/autostart/xfce4-power-manager.desktop &
-  # /run/current-system/sw/etc/xdg/autostart/org.gnome.Evolution-alarm-notify.desktop \
-  # /run/current-system/sw/etc/xdg/autostart/gnome-keyring-pkcs11.desktop \
-  # /run/current-system/sw/etc/xdg/autostart/gnome-keyring-ssh.desktop \
-  # /run/current-system/sw/etc/xdg/autostart/gnome-keyring-secrets.desktop \
-  # /run/current-system/sw/etc/xdg/autostart/xfsettingsd.desktop &
 
 xsettingsd &
 
@@ -16,22 +11,17 @@ xsettingsd &
 if [[ $(autorandr | grep "detected" | wc -l) -eq 0 ]]; then
   /etc/scripts/random-bg.sh &
   /home/gean/.config/autorandr/postswitch.d/change-dpi &
-  # /etc/scripts/polybar-start.sh &
-  # /etc/scripts/dunst-start.sh &
-  # /etc/scripts/random-bg.sh &
 else
   autorandr --change &
 fi
 
 clipmenud &
 picom --experimental-backends &
-blueberry-tray &
 caffeine -a &
 libinput-gestures -c /etc/libinput-gestures.conf &
 
 /etc/scripts/backlight.sh 20% &
-# light -S 20 &
-# brightnessctl set 20% &
 
 # sudo cpupower frequency-set -g powersave
-# sudo cpupower frequency-set -r -u 800MHz
+# sudo cpupower frequency-set --related --max 2800000
+# sudo cpupower frequency-set --related --min 800000
--- modules/sway/scripts/sway-autostart.sh
+++ modules/sway/scripts/sway-autostart.sh
@@ -12,3 +12,7 @@ libinput-gestures -c /etc/libinput-gestures.conf &
 
 /etc/scripts/backlight.sh 20% &
 
+# sudo cpupower frequency-set -g powersave
+# sudo cpupower frequency-set --related --max 2800000
+# sudo cpupower frequency-set --related --max 4000000
+# sudo cpupower frequency-set --related --min 800000
