97c537fbe55e24443ddc42c11716d87a5d6fe9d9
wochap <gean.marroquin@gmail.com>
2023-08-23 15:09:15 -0500

    fix gnomekeyring with greetd

--- config/users/mixins/gnome-keyring.nix
+++ config/users/mixins/gnome-keyring.nix
@@ -8,8 +8,11 @@ in {
 
     services.gnome.gnome-keyring.enable = true;
 
-    # Fix gnome-keyring when lightdm is enablid
+    # Fix gnome-keyring when lightdm is enabled
     security.pam.services.login.enableGnomeKeyring = true;
+
+    # Fix gnome-keyring when greetd is enabled
+    security.pam.services.greetd.enableGnomeKeyring = true;
 
     environment.loginShellInit = ''
       eval $(${pkgs.gnome.gnome-keyring}/bin/gnome-keyring-daemon --start --components=secrets,ssh,pkcs11)
