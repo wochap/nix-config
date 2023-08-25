7ae4acb89477dab6ccbd68805a0213abb061105f
wochap <gean.marroquin@gmail.com>
2023-08-25 16:44:54 -0500

    setup river

--- modules/river/scripts/river-autostart.sh
+++ modules/river/scripts/river-autostart.sh
@@ -3,12 +3,13 @@
 dex /run/current-system/sw/etc/xdg/autostart/xdg-user-dirs.desktop \
   /run/current-system/sw/etc/xdg/autostart/polkit-gnome-authentication-agent-1.desktop &
 
-# TODO: move to systemd
 # kanshi &
 
-# turn off bluetooth to save battery
-bluetoothctl power off &
-
-libinput-gestures -c /etc/libinput-gestures.conf &
+# libinput-gestures -c /etc/libinput-gestures.conf &
 
 /etc/scripts/backlight.sh 20% &
+
+# sudo cpupower frequency-set -g powersave
+# sudo cpupower frequency-set --related --max 2800000
+# sudo cpupower frequency-set --related --max 4000000
+# sudo cpupower frequency-set --related --min 800000
