65d0b807618a9dc1b3d543ff7e842e4e006f9711
wochap <gean.marroquin@gmail.com>
2023-08-25 16:31:19 -0500

    setup river

--- modules/river/default.nix
+++ modules/river/default.nix
@@ -7,15 +7,16 @@ let
   scripts = import ./scripts { inherit config pkgs lib; };
   unwrapHex = str: builtins.substring 1 (builtins.stringLength str) str;
 in {
-  options._custom.river = { enable = lib.mkEnableOption "activate river"; };
+  options._custom.river = { enable = lib.mkEnableOption { }; };
 
   config = lib.mkIf cfg.enable {
-    environment = {
-      systemPackages = with pkgs; [
-        river
+    _custom.wm.greetd = {
+      enable = lib.mkDefault true;
+      cmd = "river";
+    };
 
-        scripts.river-focus-toggle
-      ];
+    environment = {
+      systemPackages = with pkgs; [ wlopm river scripts.river-focus-toggle ];
 
       sessionVariables = {
         XDG_CURRENT_DESKTOP = "river";
@@ -29,20 +30,20 @@ in {
         };
 
         # scripts to open projects blazingly fast
-        # "scripts/projects/river-dangerp.sh" = {
-        #   source = ./scripts/river-dangerp.sh;
-        #   mode = "0755";
-        # };
+        "scripts/projects/river-dangerp.sh" = {
+          source = ./scripts/river-dangerp.sh;
+          mode = "0755";
+        };
       };
-
-      loginShellInit = lib.mkAfter ''
-        if [[ -z "$DISPLAY" ]] && [[ $(tty) = /dev/tty1 ]]; then
-          exec river
-        fi
-      '';
     };
 
     home-manager.users.${userName} = {
+      _custom.programs.waybar = {
+        settings.mainBar = {
+          modules-left = [ "river/tags" "keyboard-state" "river/mode" ];
+          modules-center = [ "river/window" ];
+        };
+      };
 
       xdg.configFile = {
         "river/init" = {
@@ -60,6 +61,5 @@ in {
         };
       };
     };
-
   };
 }
