5720904536d02d7cd83905337a0abd055d8eb71c
wochap <gean.marroquin@gmail.com>
2023-08-26 00:03:53 -0500

    feat: setup river-focus-toggle script

--- modules/river/scripts/river-focus-toggle.sh
+++ modules/river/scripts/river-focus-toggle.sh
@@ -6,17 +6,25 @@ eval set -- "$TEMP"
 shift
 target="$1"
 runstring="$2"
+tag="$3" # e.g. $((1 << 31))
 echo "$PROG: target='$target' runstring='$runstring'" >&2
 
 if [[ -z "$runstring" ]]; then
   exit
 fi
 
+# tag where the view lives
+riverctl rule-del tag -app-id "$target"
+riverctl rule-add tag -app-id "$target" "$tag"
+riverctl toggle-focused-tags "$tag"
+wlrctl window focus "$target" || riverctl spawn "$runstring"
+
 # TODO: check if it is running
 # if running move to scratchpad
 # if not, open or toggle visibility/sticky
+# https://gitlab.com/snakedye/ristate
 # https://git.sr.ht/~leon_plickat/lswt
 # https://git.sr.ht/~brocellous/wlrctl
 # https://github.com/riverwm/river/issues/350
-riverctl spawn "$runstring"
+# riverctl spawn "$runstring"
 
