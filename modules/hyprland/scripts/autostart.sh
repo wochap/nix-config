3b840c89910168fbb9ddee946e86e067096f225b
wochap <gean.marroquin@gmail.com>
2023-08-26 20:23:58 -0500

    move gnome_polkit to systemd user service

--- modules/hyprland/scripts/autostart.sh
+++ modules/hyprland/scripts/autostart.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
-dex /run/current-system/sw/etc/xdg/autostart/xdg-user-dirs.desktop \
-  /run/current-system/sw/etc/xdg/autostart/polkit-gnome-authentication-agent-1.desktop &
+dex /run/current-system/sw/etc/xdg/autostart/xdg-user-dirs.desktop &
 
 # kanshi &
 
--- modules/wm/mixins/dbus/default.nix
+++ modules/wm/mixins/dbus/default.nix
@@ -17,5 +17,21 @@ in {
     services.udisks2.enable = true;
     services.hardware.bolt.enable = true; # Thunderbolt
     services.xserver.updateDbusEnvironment = true;
+
+    systemd.user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+
+      serviceConfig = {
+        Type = "simple";
+        ExecStart =
+          "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
   };
 }
