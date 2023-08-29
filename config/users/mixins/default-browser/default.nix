2abbb0d2850c1cff7a60a60ccbff1e965d857c63
wochap <gean.marroquin@gmail.com>
2023-08-28 20:19:08 -0500

    set google as default browser

--- config/users/mixins/default-browser/default.nix
+++ config/users/mixins/default-browser/default.nix
@@ -2,10 +2,6 @@
 
 let
   userName = config._userName;
-  localPkgs = import ../../packages {
-    pkgs = pkgs;
-    lib = lib;
-  };
   http-url-handler = pkgs.unstable.makeDesktopItem {
     name = "http-url-handler";
     desktopName = "HTTP URL handler";
@@ -16,9 +12,7 @@ let
     tryExec = "/etc/scripts/open_url.sh";
     noDisplay = true;
     mimeTypes = [ "x-scheme-handler/http" "x-scheme-handler/https" ];
-    extraConfig = {
-      X-MultipleArgs = "false";
-    };
+    extraConfig = { X-MultipleArgs = "false"; };
   };
 in {
   config = {
@@ -35,17 +29,12 @@ in {
     home-manager.users.${userName} = {
       xdg.mimeApps = {
         defaultApplications = {
-          "default-web-browser" = [ "http-url-handler.desktop" ];
-          "x-scheme-handler/chrome" = [ "http-url-handler.desktop" ];
-          "x-scheme-handler/http" = [ "http-url-handler.desktop" ];
-          "x-scheme-handler/https" = [ "http-url-handler.desktop" ];
-          "x-scheme-handler/unknown" = [ "http-url-handler.desktop" ];
+          "default-web-browser" = [ "google-chrome.desktop" ];
+          "x-scheme-handler/chrome" = [ "google-chrome.desktop" ];
+          "x-scheme-handler/http" = [ "google-chrome.desktop" ];
+          "x-scheme-handler/https" = [ "google-chrome.desktop" ];
+          "x-scheme-handler/unknown" = [ "google-chrome.desktop" ];
         };
-        # associations.added = {
-        #   "text/html" = [ "http-url-handler.desktop" ];
-        #   "x-scheme-handler/http" = [ "http-url-handler.desktop" ];
-        #   "x-scheme-handler/https" = [ "http-url-handler.desktop" ];
-        # };
       };
     };
   };
