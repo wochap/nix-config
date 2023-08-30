6b10fc927b54e77549a463daafe8ec9aa5951ba9
wochap <gean.marroquin@gmail.com>
2023-08-30 16:20:23 -0500

    f

--- modules/dwl/dotfiles/config.def.h
+++ modules/dwl/dotfiles/config.def.h
@@ -9,7 +9,7 @@ static const float focuscolor[]            = {1.0, 0.0, 0.0, 1.0};
 static const char cursortheme[]            = "capitaine-cursors"; /* theme from /usr/share/cursors/xorg-x11 */
 static const unsigned int cursorsize       = 24;
 /* To conform the xdg-protocol, set the alpha to zero to restore the old behavior */
-static const float fullscreen_bg[]         = {0.1, 0.1, 0.1, 1.0};
+static const float fullscreen_bg[]         = {0.1, 0.1, 0.1, 0};
 
 /* Autostart */
 static const char *const autostart[] = {
