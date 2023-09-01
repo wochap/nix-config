591ba9301757ac85f5ea6fdb42b286cc9798c8c6
wochap <gean.marroquin@gmail.com>
2023-09-01 14:08:40 -0500

    fix swww service

--- modules/wayland-wm/mixins/swww/default.nix
+++ modules/wayland-wm/mixins/swww/default.nix
@@ -9,11 +9,12 @@ let
     executable = true;
     text = builtins.readFile ./scripts/random-bg.sh;
   };
+  inherit (pkgs.unstable) swww;
 in {
   config = lib.mkIf cfg.enable {
     home-manager.users.${userName} = {
       home = {
-        packages = with pkgs; [ swww swww-random ];
+        packages = [ swww swww-random ];
         sessionVariables = {
           SWWW_TRANSITION_TYPE = "simple";
           SWWW_TRANSITION_STEP = "45";
@@ -32,14 +33,20 @@ in {
 
         Service = {
           PassEnvironment = [
+            # "HOME"
+            "PATH"
+            "XDG_RUNTIME_DIR"
             "SWWW_TRANSITION_TYPE"
             "SWWW_TRANSITION_STEP"
             "SWWW_TRANSITION_FPS"
             "SWWW_TRANSITION_BEZIER"
           ];
-          ExecStart = "${pkgs.swww}/bin/swww-daemon";
-          ExecStop = "${pkgs.swww}/bin/swww kill";
+          ExecStart = "${swww}/bin/swww init";
+          # ExecStartPost = "${swww}/bin/swww img $(${pkgs.coreutils}/bin/cat $HOME/.cache/swww/* | ${pkgs.coreutils}/bin/head -n 1)";
+          # ExecStartPost = "${swww-random}/bin/swww-random $HOME/Pictures/backgrounds/";
+          ExecStop = "${swww}/bin/swww kill";
           Type = "oneshot";
+          RemainAfterExit = true;
           KillMode = "mixed";
         };
 
