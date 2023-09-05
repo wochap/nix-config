8de96ad11e2fbd766255d74431381c709a12c09f
wochap <gean.marroquin@gmail.com>
2023-09-04 23:07:03 -0500

    add unwrapHex

--- config/packages/default.nix
+++ config/packages/default.nix
@@ -1,17 +1,20 @@
 { pkgs, lib }:
 
 {
-  advcpmv = pkgs.callPackage ./advcpmv {};
-  fromYAML = pkgs.callPackage ./from-yaml.nix {};
-  interception-both-shift-capslock = pkgs.callPackage ./interception-both-shift-capslock {};
-  mailnotify = pkgs.callPackage ./mailnotify.nix {};
-  mangadesk = pkgs.callPackage ./mangadesk.nix {};
-  nwg-look = pkgs.callPackage ./nwg-look {};
-  offlinemsmtp = pkgs.callPackage ./offlinemsmtp.nix {};
-  ptsh = pkgs.callPackage ./ptsh {};
-  sf-mono-liga-bin = pkgs.callPackage ./sf-mono-liga-bin.nix {};
-  usbfluxd = pkgs.callPackage ./usbfluxd.nix {};
-  customNodePackages = lib.dontRecurseIntoAttrs (pkgs.callPackage ./custom-node-packages {
-    nodejs = pkgs.prevstable-nodejs.nodejs-14_x;
-  });
+  unwrapHex = str: builtins.substring 1 (builtins.stringLength str) str;
+  advcpmv = pkgs.callPackage ./advcpmv { };
+  fromYAML = pkgs.callPackage ./from-yaml.nix { };
+  interception-both-shift-capslock =
+    pkgs.callPackage ./interception-both-shift-capslock { };
+  mailnotify = pkgs.callPackage ./mailnotify.nix { };
+  mangadesk = pkgs.callPackage ./mangadesk.nix { };
+  nwg-look = pkgs.callPackage ./nwg-look { };
+  offlinemsmtp = pkgs.callPackage ./offlinemsmtp.nix { };
+  ptsh = pkgs.callPackage ./ptsh { };
+  sf-mono-liga-bin = pkgs.callPackage ./sf-mono-liga-bin.nix { };
+  usbfluxd = pkgs.callPackage ./usbfluxd.nix { };
+  customNodePackages = lib.dontRecurseIntoAttrs
+    (pkgs.callPackage ./custom-node-packages {
+      nodejs = pkgs.prevstable-nodejs.nodejs-14_x;
+    });
 }
