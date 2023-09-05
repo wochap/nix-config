ff412212377b115cb9e18c0e93d7fe865240652b
wochap <gean.marroquin@gmail.com>
2023-09-04 23:06:50 -0500

    rice vte console

--- config/mixins/nixos.nix
+++ config/mixins/nixos.nix
@@ -2,24 +2,26 @@
 
 let
   inherit (config._custom.globals) isHidpi;
-  terminalColors = [
-    "21222c"
-    "ff5555"
-    "50fa7b"
-    "f1fa8c"
-    "bd93f9"
-    "ff79c6"
-    "8be9fd"
-    "f8f8f2"
 
-    "6272a4"
-    "ff6e6e"
-    "69ff94"
-    "ffffa5"
-    "d6acff"
-    "ff92df"
-    "a4ffff"
-    "ffffff"
+  # Catppuccin mocha theme
+  terminalColors = [
+    "45475A"
+    "F38BA8"
+    "A6E3A1"
+    "F9E2AF"
+    "89B4FA"
+    "F5C2E7"
+    "94E2D5"
+    "BAC2DE"
+
+    "585B70"
+    "F38BA8"
+    "A6E3A1"
+    "F9E2AF"
+    "89B4FA"
+    "F5C2E7"
+    "94E2D5"
+    "A6ADC8"
   ];
 in {
   imports = [
