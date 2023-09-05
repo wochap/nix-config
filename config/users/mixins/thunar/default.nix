efcbb62faaed2acb6046be4f9dce9a5f825096c8
wochap <gean.marroquin@gmail.com>
2023-09-05 12:51:18 -0500

    fix thunar preview thumbnails

--- config/users/mixins/thunar/default.nix
+++ config/users/mixins/thunar/default.nix
@@ -8,14 +8,23 @@ in {
       plugins = with pkgs; [
         pkgs.xfce.thunar-archive-plugin
         xfce.thunar-media-tags-plugin
+        xfce.thunar-volman # auto mont devices
       ];
     };
 
     environment = {
-      systemPackages = with pkgs;
-        [
-          xfce.thunar-volman # auto mont devices
-        ];
+      systemPackages = with pkgs; [
+
+        # required by tumbler service
+        # TODO: add https://gitlab.com/hxss-linux/folderpreview
+        ffmpegthumbnailer # videos
+        freetype # fonts
+        gdk-pixbuf # images
+        libgepub # .epub
+        libgsf # .odf
+        poppler # .pdf .ps
+        webp-pixbuf-loader # .webp
+      ];
     };
 
     services.gvfs.enable = true; # Mount, trash, and other functionalities
