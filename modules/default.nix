a68ce69c61a6fccd102e2c35f2af143cddd1aa8a
wochap <gean.marroquin@gmail.com>
2023-09-10 17:45:27 -0500

    feat: add gnome

--- modules/default.nix
+++ modules/default.nix
@@ -7,6 +7,7 @@
     ./dwl
     ./flatpak
     ./globals.nix
+    ./gnome
     ./gui
     ./hardware
     ./hyprland
---
+++ modules/gnome/default.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, lib, ... }:
+
+let
+  userName = config._userName;
+  cfg = config._custom.gnome;
+in {
+  options._custom.gnome = { enable = lib.mkEnableOption { }; };
+
+  config = lib.mkIf cfg.enable {
+    environment.gnome.excludePackages = with pkgs; [ epiphany ];
+
+    services.xserver.desktopManager.gnome.enable = true;
+    services.gnome.games.enable = false;
+  };
+}
