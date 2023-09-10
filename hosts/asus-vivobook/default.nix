4bd431080966e08ccc909aff15a55bad918db46c
wochap <gean.marroquin@gmail.com>
2023-09-10 17:42:28 -0500

    f

--- hosts/asus-vivobook/default.nix
+++ hosts/asus-vivobook/default.nix
@@ -3,16 +3,61 @@
 let
   hostName = "vivobook";
   userName = "franshesca";
-in
-{
+  hmConfig = config.home-manager.users.${userName};
+  configDirectory = "${hmConfig.home.homeDirectory}/nix-config";
+  catppuccinMochaTheme = import ../../config/mixins/catppuccin-mocha.nix;
+in {
   imports = [
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+    inputs.nixos-hardware.nixosModules.common-gpu-amd
     /etc/nixos/hardware-configuration.nix
-    ../../config/mixins/kmonad
-    ../../config/wayland.nix
+    ../../config/nixos.nix
   ];
 
   config = {
     _userName = userName;
+    _homeDirectory = "/home/${userName}";
+    _configDirectory = configDirectory;
+    _custom.globals.themeColors = catppuccinMochaTheme;
+    _custom.globals.isHidpi = false;
+
+    _custom.tui.nixDirenv.enable = true;
+
+    _custom.gui.qutebrowser.enable = false;
+
+    _custom.wm.email.enable = false;
+    _custom.wm.calendar.enable = false;
+    _custom.wm.networking.enable = true;
+    _custom.wm.powerManagement.enable = false;
+    _custom.wm.backlight.enable = false;
+    _custom.wm.audio.enable = false;
+    _custom.wm.cursor.enable = false;
+    _custom.wm.xdg.enable = false;
+    _custom.wm.dbus.enable = false;
+    _custom.wm.bluetooth.enable = false;
+    _custom.wm.qt.enable = false;
+    _custom.wm.gtk.enable = false;
+
+    _custom.interception-tools.enable = false;
+    _custom.steam.enable = true;
+    _custom.mbpfan.enable = false;
+    _custom.docker.enable = true;
+    _custom.mongodb.enable = true;
+    _custom.virt.enable = false;
+    _custom.flatpak.enable = true;
+    _custom.waydroid.enable = true;
+
+    _custom.hardware.efi.enable = true;
+    _custom.hardware.amdCpu.enable = true;
+    _custom.hardware.amdGpu.enable = false;
+    _custom.hardware.amdGpu.enableSouthernIslands = false;
+
+    _custom.gnome.enable = false;
+    _custom.dwl.enable = false;
+    _custom.river.enable = false;
+    _custom.hyprland.enable = false;
+    _custom.sway.enable = false;
+    _custom.waylandWm.enable = false;
 
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
@@ -33,6 +78,11 @@ in
     home-manager.users.${userName}.home.stateVersion = "21.03";
 
     boot = {
+      # kernelParams = [
+      #   # needed function keys
+      #   "hid_apple.fnmode=2"
+      # ];
+      # kernelModules = [ "hid-apple" ];
       loader = {
         grub.enable = false;
         systemd-boot.enable = true;
@@ -42,7 +92,7 @@ in
     };
 
     networking = {
-      hostName = hostName;
+      inherit hostName;
 
       # The global useDHCP flag is deprecated, therefore explicitly set to false here.
       # Per-interface useDHCP will be mandatory in the future, so this generated config
@@ -55,12 +105,16 @@ in
     time.hardwareClockInLocalTime = true;
 
     services.xserver = {
-      libinput.enable = true;
 
       # Setup keyboard
       layout = "latam";
       xkbModel = "pc104";
       # xkbVariant = "altgr-intl";
+
+      # Enable touchpad support (enabled default in most desktopManager).
+      libinput.enable = true;
+      libinput.touchpad.naturalScrolling = true;
+      libinput.touchpad.tapping = false;
     };
   };
 }
