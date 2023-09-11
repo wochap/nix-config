8f1e2dc431ccc824e9fd03873facad604d61cc9c
wochap <gean.marroquin@gmail.com>
2023-09-10 22:52:09 -0500

    f

--- modules/gnome/default.nix
+++ modules/gnome/default.nix
@@ -9,9 +9,21 @@ in {
   config = lib.mkIf cfg.enable {
     environment.gnome.excludePackages = with pkgs; [ epiphany ];
 
+    environment.systemPackages = with pkgs; [
+      gnomeExtensions.appindicatora
+      gnomeExtensions.blur-my-shell
+      gnomeExtensions.clipboard-indicator
+      gnomeExtensions.dash-to-panel
+      gnomeExtensions.tweaks-in-system-menu
+      gnomeExtensions.workspace-indicator
+      gnomeExtensions.x11-gestures
+    ];
+
+    services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+    services.gnome.games.enable = false;
+    services.gnome.gnome-browser-connector.enable = true;
     services.xserver.displayManager.lightdm.enable = false;
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
-    services.gnome.games.enable = false;
   };
 }
