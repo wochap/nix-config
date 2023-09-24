a528434e63bfa801339dcbdd47e2478ae3d61560
wochap <gean.marroquin@gmail.com>
2023-09-24 00:37:30 -0500

    mount ios devices

--- config/users/mixins/thunar/default.nix
+++ config/users/mixins/thunar/default.nix
@@ -28,6 +28,7 @@ in {
     };
 
     services.gvfs.enable = true; # Mount, trash, and other functionalities
+    services.gvfs.package = pkgs.gnome.gvfs;
     services.tumbler.enable = true; # Thumbnail support for images
 
     home-manager.users.${userName} = {
