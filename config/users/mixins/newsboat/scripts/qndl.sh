62c5578d709a6f3c7c67fe4e776afae622671bd7
wochap <gean.marroquin@gmail.com>
2023-10-01 09:40:17 -0500

    rice

--- config/users/mixins/newsboat/scripts/qndl.sh
+++ config/users/mixins/newsboat/scripts/qndl.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# $1 is a url; $2 is a command
+# $1 is a url; $2 is a opt command to download the url
 [ -z "$1" ] && exit
 base="$(basename "$1")"
-notify-send "⏳ Queuing $base..."
+notify-send "Download" "⏳ Queuing $base..." -a "qndl" -i "media-downloader"
 cmd="$2"
 [ -z "$cmd" ] && cmd="youtube-dl --add-metadata -ic"
 idnum="$(tsp $cmd "$1")"
 realname="$(echo "$base" | sed "s/?\(source\|dest\).*//;s/%20/ /g")"
 tsp -D "$idnum" mv "$base" "$realname"
-tsp -D "$idnum" notify-send "👍 $realname done."
+tsp -D "$idnum" notify-send "Download" "👍 $realname done." -a "qndl" -i "media-downloader"
