20ec000e816dc0978ec209ba119074a69cd7fd3f
wochap <gean.marroquin@gmail.com>
2023-11-07 21:17:24 -0500

    fix

--- modules/interception-tools/default.nix
+++ modules/interception-tools/default.nix
@@ -3,6 +3,8 @@
 let
   userName = config._userName;
   cfg = config._custom.interception-tools;
+  DEVNODE =
+    "/dev/input/by-id/usb-Apple_Inc._Apple_Internal_Keyboard___Trackpad_D3H54961U11GHMFA75BS-if01-event-kbd";
 in {
   options._custom.interception-tools = { enable = lib.mkEnableOption { }; };
 
@@ -13,7 +15,7 @@ in {
       _custom.interception-both-shift-capslock
     ];
     services.interception-tools.udevmonConfig = with pkgs; ''
-      - JOB: "${interception-tools}/bin/intercept -g $DEVNODE | ${interception-tools-plugins.caps2esc}/bin/caps2esc -m 1 | ${_custom.interception-both-shift-capslock}/bin/both-shift-capslock | ${interception-tools}/bin/uinput -d $DEVNODE"
+      - JOB: "${interception-tools}/bin/intercept -g '${DEVNODE}' | ${interception-tools-plugins.caps2esc}/bin/caps2esc -m 1 | ${_custom.interception-both-shift-capslock}/bin/both-shift-capslock | ${interception-tools}/bin/uinput -d '${DEVNODE}'"
         DEVICE:
           EVENTS:
             EV_KEY: [KEY_CAPSLOCK, KEY_LEFTSHIFT, KEY_RIGHTSHIFT]
