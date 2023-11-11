afe5e2de39b420711839ddfe387fe3c0ea6022a0
wochap <gean.marroquin@gmail.com>
2023-11-11 17:39:42 -0500

    refactor

--- config/users/user-nixos.nix
+++ config/users/user-nixos.nix
@@ -1,45 +1,13 @@
-{ config, pkgs, lib, inputs, ... }:
+{ config, ... }:
 
 let userName = config._userName;
 in {
   imports = [
-    ./mixins/alacritty
-    ./mixins/amfora
-    ./mixins/bat
-    ./mixins/bottom.nix
     ./mixins/default-browser
-    ./mixins/discord
-    ./mixins/firefox
-    ./mixins/fonts
-    ./mixins/fzf.nix
-    ./mixins/gammastep.nix
-    ./mixins/git
-    ./mixins/gnome-keyring.nix
-    ./mixins/gpg.nix
-    ./mixins/htop.nix
-    ./mixins/imv.nix
-    ./mixins/kitty
-    ./mixins/lsd.nix
-    ./mixins/mangadesk
-    ./mixins/mangal
-    ./mixins/mpv
-    ./mixins/music
-    ./mixins/neofetch
-    ./mixins/newsboat
     ./mixins/nixos
-    ./mixins/nnn
-    ./mixins/ptsh
     ./mixins/secrets.nix
-    ./mixins/ssh
-    ./mixins/starship.nix
-    ./mixins/syncthing.nix
-    ./mixins/thunar
     ./mixins/user-nix.nix
     ./mixins/user-nixos.nix
-    ./mixins/vim
-    ./mixins/youtube.nix
-    ./mixins/zathura
-    ./mixins/zsh
   ];
 
   config = {
