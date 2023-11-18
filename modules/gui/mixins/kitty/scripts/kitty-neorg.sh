aa33afbd72002344eee2daa29a9d06a0329d6855
wochap <gean.marroquin@gmail.com>
2023-11-18 16:55:49 -0500

    load last session

--- modules/gui/mixins/kitty/scripts/kitty-neorg.sh
+++ modules/gui/mixins/kitty/scripts/kitty-neorg.sh
@@ -5,5 +5,5 @@ uname=$(uname)
 if [[ "$uname" == "Darwin" ]]; then
   kitty --title kitty-neorg -o window_padding_width=0 -e nvim +'cd ~/Sync/neorg' +'Neorg workspace home'
 else
-  kitty --class kitty-neorg --title neorg -o window_padding_width=0 -e nvim +'cd ~/Sync/neorg'
+  kitty --class kitty-neorg --title neorg -o window_padding_width=0 -e nvim +'cd ~/Sync/neorg' +":lua require('persistence').load()"
 fi
