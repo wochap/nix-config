1004bac1ebcd84964c56d282c6dcb5a4db36c148
wochap <gean.marroquin@gmail.com>
2023-12-02 14:27:12 -0500

    add fmpc

--- modules/cli/mixins/zsh/functions.zsh
+++ modules/cli/mixins/zsh/functions.zsh
@@ -40,6 +40,15 @@ function fns() {
   script=$(cat package.json | jq -r '.scripts | keys[] ' | sort | fzf) && npm run $(echo "$script")
 }
 
+# play song with mpc
+function fmpc() {
+  local song_position
+  song_position=$(mpc -f "%position%) %artist% - %title%" playlist | \
+    fzf-tmux --query="$1" --reverse --select-1 --exit-0 | \
+    sed -n 's/^\([0-9]\+\)).*/\1/p') || return 1
+  [ -n "$song_position" ] && mpc -q play $song_position
+}
+
 # HACK: remove padding inside kitty
 function run-without-kpadding() {
   if [[ -n $KITTY_PID ]]; then
