ce693370bab144ba625950292429841a27853c37
wochap <gean.marroquin@gmail.com>
2023-12-02 14:27:02 -0500

    feat(fzf) add mappings

--- modules/cli/mixins/fzf.nix
+++ modules/cli/mixins/fzf.nix
@@ -8,18 +8,27 @@ in {
 
   config = lib.mkIf cfg.enable {
     home-manager.users.${userName} = {
-      home.sessionVariables = {
-        # setup https://github.com/catppuccin/fzf
-        FZF_DEFAULT_OPTS =
-          "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8";
-      };
-
       programs.fzf = {
         enable = true;
         package = pkgs.unstable.fzf;
         enableBashIntegration = true;
+        defaultOptions = [
+          # setup https://github.com/catppuccin/fzf
+          "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8"
+          "--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc"
+          "--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
+          # mappings
+          "--bind 'ctrl-d:preview-half-page-down"
+          "--bind 'ctrl-u:preview-half-page-up'"
+          "--bind 'ctrl-e:abort'"
+          "--bind 'ctrl-y:accept'"
+          "--bind 'ctrl-f:half-page-down'"
+          "--bind 'ctrl-b:half-page-up'"
+        ];
         # CTRL-T - Paste the selected file path(s) into the command line
-        fileWidgetOptions = [ "--preview 'head {}'" ];
+        fileWidgetOptions =
+          [ "--preview 'bat --style=numbers --color=always {}'" ];
         # CTRL-R - Paste the selected command from history into the command line
         historyWidgetOptions = [ ];
         # ALT-C - cd into the selected directory
