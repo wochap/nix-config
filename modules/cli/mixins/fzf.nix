8a29adc7bb9027b13f4679ed15f1915e6a5fdd72
wochap <gean.marroquin@gmail.com>
2023-12-03 14:37:09 -0500

    rice fzf

--- modules/cli/mixins/fzf.nix
+++ modules/cli/mixins/fzf.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, inputs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   cfg = config._custom.cli.fzf;
@@ -13,26 +13,41 @@ in {
         package = pkgs.unstable.fzf;
         enableBashIntegration = true;
         defaultOptions = [
-          # setup https://github.com/catppuccin/fzf
+          # theme
+          # source: https://github.com/catppuccin/fzf
           "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8"
           "--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc"
           "--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
+          "--no-height"
+          "--tabstop=2"
+          "--cycle"
+          "--layout=default"
+
           # mappings
-          "--bind 'ctrl-d:preview-half-page-down"
+          "--bind 'ctrl-d:preview-half-page-down'"
           "--bind 'ctrl-u:preview-half-page-up'"
           "--bind 'ctrl-e:abort'"
           "--bind 'ctrl-y:accept'"
           "--bind 'ctrl-f:half-page-down'"
           "--bind 'ctrl-b:half-page-up'"
+          "--bind '?:toggle-preview'"
         ];
         # CTRL-T - Paste the selected file path(s) into the command line
-        fileWidgetOptions =
-          [ "--preview 'bat --style=numbers --color=always {}'" ];
+        fileWidgetOptions = [
+          "--preview '(bat --style=numbers --color=always {} || lsd -l -A --ignore-glob=.git --tree --depth=2 --color=always --blocks=size,name {}) 2> /dev/null | head -200'"
+          "--preview-window down:50%"
+        ];
         # CTRL-R - Paste the selected command from history into the command line
-        historyWidgetOptions = [ ];
+        historyWidgetOptions = [
+          "--preview 'echo {} | sed \\\"s/^ *\\([0-9|*]\\+\\) *//\\\" | bat --plain --language=sh --color=always'"
+          "--preview-window down:50%:wrap"
+        ];
         # ALT-C - cd into the selected directory
-        changeDirWidgetOptions = [ "--preview 'tree -C {} | head -100'" ];
+        changeDirWidgetOptions = [
+          "--preview 'lsd -l -A --ignore-glob=.git --tree --depth=2 --color=always --blocks=size,name {} | head -200'"
+          "--preview-window down:50%"
+        ];
       };
     };
   };
