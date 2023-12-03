f84775d39ca22bd7552356ec019500c47952254d
wochap <gean.marroquin@gmail.com>
2023-12-03 17:17:48 -0500

    fix cursor position

--- modules/gui/mixins/kitty/scripts/nvim-scrollback.sh
+++ modules/gui/mixins/kitty/scripts/nvim-scrollback.sh
@@ -14,7 +14,7 @@ fi
 
 exec nvim \
   -c "map <silent> q :qa!<CR>" \
-  -c "set shell=bash scrollback=100000 laststatus=0 cmdheight=0 filetype=terminal" \
+  -c "set shell=bash scrollback=100000 laststatus=0 cmdheight=0 filetype=terminal scrolloff=0" \
   -c "autocmd TermEnter * stopinsert" \
   -c "autocmd TermClose * ${AUTOCMD_TERMCLOSE_CMD}" \
   -c 'terminal sed </dev/fd/63 -e "s/'$'\x1b'']8;;file:[^\]*[\]//g" && sleep 0.01 && printf "'$'\x1b'']2;"' 63<&0 0</dev/null
