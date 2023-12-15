128af2db38fed6a366b204e23d19dda6648f077a
wochap <gean.marroquin@gmail.com>
2023-12-15 04:16:05 -0500

    clean

--- modules/cli/mixins/zsh/key-bindings-vi.zsh
+++ modules/cli/mixins/zsh/key-bindings-vi.zsh
@@ -49,6 +49,9 @@ multibindkey 'visual vicmd viopp' "${terminfo[kcud1]}" .down-line
 # [Esc] - remove key binding to espace to vicmd, use Ctrl+x
 bindkey -M viins -r '^['
 
+# [Ctrl+l] - remove key binding
+bindkey -M viins -r '^L'
+
 # [Ctrl+c] -
 multibindkey 'viins visual vicmd viopp menuselect' '^C' send-break
 
