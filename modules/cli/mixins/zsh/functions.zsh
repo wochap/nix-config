a9eabd9087d247a60f9f419b180989ec5170b078
wochap <gean.marroquin@gmail.com>
2023-12-15 21:08:46 -0500

    hotfix(zsh)

--- modules/cli/mixins/zsh/functions.zsh
+++ modules/cli/mixins/zsh/functions.zsh
@@ -146,34 +146,37 @@ function f()
   fi
 }
 
-# transient prompt for starship
-zle-line-init() {
-  emulate -L zsh
-
-  [[ $CONTEXT == start ]] || return 0
-
-  while true; do
-    zle .recursive-edit
-    local -i ret=$?
-    [[ $ret == 0 && $KEYS == $'\4' ]] || break
-    [[ -o ignore_eof ]] || exit 0
-  done
-
-  local saved_prompt=$PROMPT
-  local saved_rprompt=$RPROMPT
-  PROMPT='$(STARSHIP_CONFIG=~/.config/starship-transient.toml starship prompt --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="$STARSHIP_CMD_STATUS" --pipestatus="${STARSHIP_PIPE_STATUS[*]}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
-  RPROMPT=''
-  zle .reset-prompt
-  PROMPT=$saved_prompt
-  RPROMPT=$saved_rprompt
-
-  if (( ret )); then
-    zle .send-break
-  else
-    zle .accept-line
-  fi
-  return ret
-}
-
-zle -N zle-line-init
+# TODO: fix bug with zsh-vi-mode
+# can't enter vicmd mode if you do Ctrl+c when in vicmd
+# TODO: it also hide dotfiles in complete menu
+# # transient prompt for starship
+# zle-line-init() {
+#   emulate -L zsh
+#
+#   [[ $CONTEXT == start ]] || return 0
+#
+#   while true; do
+#     zle .recursive-edit
+#     local -i ret=$?
+#     [[ $ret == 0 && $KEYS == $'\4' ]] || break
+#     [[ -o ignore_eof ]] || exit 0
+#   done
+#
+#   local saved_prompt=$PROMPT
+#   local saved_rprompt=$RPROMPT
+#   PROMPT='$(STARSHIP_CONFIG=~/.config/starship-transient.toml starship prompt --terminal-width="$COLUMNS" --keymap="${KEYMAP:-}" --status="$STARSHIP_CMD_STATUS" --pipestatus="${STARSHIP_PIPE_STATUS[*]}" --cmd-duration="${STARSHIP_DURATION:-}" --jobs="$STARSHIP_JOBS_COUNT")'
+#   RPROMPT=''
+#   zle .reset-prompt
+#   PROMPT=$saved_prompt
+#   RPROMPT=$saved_rprompt
+#
+#   if (( ret )); then
+#     zle .send-break
+#   else
+#     zle .accept-line
+#   fi
+#   return ret
+# }
+#
+# zle -N zle-line-init
 
