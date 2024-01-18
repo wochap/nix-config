1736c3ffa7a241fff73c89d93bf91d5a23b76dfb
wochap <gean.marroquin@gmail.com>
2024-01-18 10:05:04 -0500

    use nvidia prime offload mode instead of sync mode

--- hosts/legion/hardware.nix
+++ hosts/legion/hardware.nix
@@ -2,16 +2,36 @@
 
 {
   imports = [
-    inputs.nixos-hardware.nixosModules.lenovo-legion-16aph8
+    inputs.nixos-hardware.nixosModules.common-cpu-amd
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-raphael-igpu
+    inputs.nixos-hardware.nixosModules.common-gpu-amd
+    inputs.nixos-hardware.nixosModules.common-gpu-nvidia
+    inputs.nixos-hardware.nixosModules.common-pc-laptop
+    inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
     inputs.nixos-hardware.nixosModules.common-hidpi
   ];
 
   config = {
     hardware.amdgpu.amdvlk = true;
     hardware.amdgpu.loadInInitrd = true;
+    hardware.amdgpu.opencl = true;
 
-    boot.extraModulePackages = with config.boot.kernelPackages;
-      [ lenovo-legion-module ];
+    hardware.nvidia = {
+      modesetting.enable = true;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+    hardware.nvidia.prime = {
+      amdgpuBusId = "PCI:5:0:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+
+    # Doesn't work yet...
+    # boot.extraModulePackages = with config.boot.kernelPackages;
+    #   [ lenovo-legion-module ];
+
+    boot.kernelPackages = lib.mkForce pkgs.unstable.linuxPackages_latest;
 
     environment.systemPackages = with pkgs; [
       lenovo-legion
@@ -21,10 +41,14 @@
     environment.sessionVariables = {
       IGPU_CARD = "$(readlink -f /dev/dri/by-path/pci-0000:05:00.0-card)";
       DGPU_CARD = "$(readlink -f /dev/dri/by-path/pci-0000:01:00.0-card)";
-      WLR_DRM_DEVICES = "$IGPU_CARD:$DGPU_CARD";
+      WLR_DRM_DEVICES = "$IGPU_CARD";
     };
 
     # zramSwap.enable = true;
+
+    # AMD has better battery life with PPD over TLP:
+    # https://community.frame.work/t/responded-amd-7040-sleep-states/38101/13
+    services.power-profiles-daemon.enable = lib.mkDefault true;
 
     services.thermald.enable = lib.mkDefault true;
 
