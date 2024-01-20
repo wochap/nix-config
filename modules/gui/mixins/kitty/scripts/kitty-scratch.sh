9e80a08f734935909c7b2068d4d224d6e6e54e4a
wochap <gean.marroquin@gmail.com>
2024-01-20 14:19:52 -0500

    remove support for darwin

--- modules/gui/mixins/kitty/scripts/kitty-scratch.sh
+++ modules/gui/mixins/kitty/scripts/kitty-scratch.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-uname=$(uname)
-
-if [[ "$uname" == "Darwin" ]]; then
-  kitty --title kitty-scratch --listen-on "unix:/tmp/kitty_scratch"
-else
-  kitty --class kitty-scratch --listen-on "unix:/tmp/kitty_scratch"
-fi
-
+kitty --class kitty-scratch --listen-on "unix:/tmp/kitty_scratch"
