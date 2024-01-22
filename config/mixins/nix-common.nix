84019691ad4fa818bcb2c99ab127ca53a613aa19
wochap <gean.marroquin@gmail.com>
2024-01-21 22:45:41 -0500

    add cuda cachix

--- config/mixins/nix-common.nix
+++ config/mixins/nix-common.nix
@@ -36,23 +36,25 @@ in {
         # Enable cachix
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-          "colemickens.cachix.org-1:bNrJ6FfMREB4bd4BOjEN85Niu8VcPdQe4F4KxVsb/I4="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
           "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
           "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
           "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
           "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+          "colemickens.cachix.org-1:bNrJ6FfMREB4bd4BOjEN85Niu8VcPdQe4F4KxVsb/I4="
         ];
         substituters = [
           "https://cache.nixos.org"
-          "https://colemickens.cachix.org"
           "https://nix-community.cachix.org"
+          "https://cuda-maintainers.cachix.org"
           "https://nixpkgs-wayland.cachix.org"
           "https://hyprland.cachix.org"
           "https://nixpkgs-python.cachix.org"
           "https://cache.garnix.io"
           "https://devenv.cachix.org"
+          "https://colemickens.cachix.org"
         ];
 
         trusted-users = [ "@wheel" "root" ];
