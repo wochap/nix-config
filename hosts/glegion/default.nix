a495035ba07d13b72eb046f6a55a4c33ffd8de3e
wochap <gean.marroquin@gmail.com>
2024-01-31 16:32:16 +0000

    add timezone

--- hosts/glegion/default.nix
+++ hosts/glegion/default.nix
@@ -110,6 +110,8 @@ in {
       libinput.touchpad.tapping = true;
     };
 
+    time.timeZone = "America/Panama";
+
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It‘s perfectly fine and recommended to leave
