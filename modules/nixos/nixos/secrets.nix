5dc5f41e809619c226850ab37919d1b16e37416a
wochap <gean.marroquin@gmail.com>
2024-01-31 11:33:24 -0500

    style

--- modules/nixos/nixos/secrets.nix
+++ modules/nixos/nixos/secrets.nix
@@ -2,12 +2,6 @@
 
 let inherit (config._custom.globals) configDirectory;
 in {
-  config = {
-    _custom.hm = {
-      xdg.configFile = {
-        "secrets".source =
-          lib._custom.mkOutOfStoreSymlink "${configDirectory}/secrets";
-      };
-    };
-  };
+  config._custom.hm.xdg.configFile."secrets".source =
+    lib._custom.mkOutOfStoreSymlink "${configDirectory}/secrets";
 }
