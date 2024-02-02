4ea67dffd35338092f85026c66d6356b633f5d2b
wochap <gean.marroquin@gmail.com>
2024-02-01 20:22:01 -0500

    make syncthing more secure?

--- modules/nixos/services/syncthing/default.nix
+++ modules/nixos/services/syncthing/default.nix
@@ -2,18 +2,18 @@
 
 let cfg = config._custom.services.syncthing;
 in {
-  options._custom.services.syncthing = { enable = lib.mkEnableOption { }; };
+  options._custom.services.syncthing.enable = lib.mkEnableOption { };
 
   config = lib.mkIf cfg.enable {
     _custom.hm = {
       services.syncthing = {
         enable = true;
-        # openDefaultPorts = true;
-        # relay.enable = false;
-        # settings.options = {
-        #   urAccepted = -1;
-        #   globalDiscoveryEnabled = false;
-        # };
+        openDefaultPorts = true;
+        relay.enable = false;
+        settings.options = {
+          urAccepted = -1;
+          globalDiscoveryEnabled = false;
+        };
       };
     };
   };
