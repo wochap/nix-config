ee4242c8912496ac9766262915b3888ce2299adb
wochap <gean.marroquin@gmail.com>
2024-02-03 16:32:18 -0500

    fix kde home manager activation

--- modules/nixos/de/kde/default.nix
+++ modules/nixos/de/kde/default.nix
@@ -7,7 +7,12 @@ in {
   config = lib.mkIf cfg.enable {
     services.xserver.enable = true;
     services.xserver.displayManager.sddm.enable = true;
+    services.xserver.displayManager.sddm.wayland.enable = true;
     services.xserver.desktopManager.plasma5.enable = true;
+
+    # Fix home-manager activation
+    # source: https://github.com/nix-community/home-manager/issues/3113
+    programs.dconf.enable = true;
   };
 }
 
