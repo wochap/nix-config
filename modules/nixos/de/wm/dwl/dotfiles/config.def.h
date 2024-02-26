839d90107132b314569860e7394f52680b1fa1ac
wochap <gean.marroquin@gmail.com>
2024-02-26 17:28:08 -0500

    enable tap and drag

--- modules/nixos/de/wm/dwl/dotfiles/config.def.h
+++ modules/nixos/de/wm/dwl/dotfiles/config.def.h
@@ -164,7 +164,7 @@ static const char *kblayouts[] = {"us", "ru"};
 
 /* Trackpad */
 static const int tap_to_click = 1;
-static const int tap_and_drag = 0;
+static const int tap_and_drag = 1;
 static const int drag_lock = 0;
 static const int natural_scrolling = 1;
 static const int disable_while_typing = 1;
