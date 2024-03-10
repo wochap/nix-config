7aad3db6194ae230bfb40a0535ef4c9ff79ebfc6
wochap <gean.marroquin@gmail.com>
2024-03-10 15:27:19 -0500

    remove time and battery info from zsh theme

--- modules/shared/programs/cli/zsh/.p10k.zsh
+++ modules/shared/programs/cli/zsh/.p10k.zsh
@@ -108,13 +108,13 @@ source /etc/scripts/theme-colors.sh
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
     # per_directory_history   # Oh My Zsh per-directory-history local/global indicator
     # cpu_arch              # CPU architecture
-    time                    # current time
+    # time                    # current time
     # =========================[ Line #2 ]=========================
     newline
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
-    battery               # internal battery
+    # battery               # internal battery
     # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
   )
