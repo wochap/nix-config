608eed5b47961e198db120f7999fbc7397ed3803
wochap <gean.marroquin@gmail.com>
2024-03-15 08:09:42 -0500

    feat: remove logs from boot process

--- modules/nixos/de/wm-addons/plymouth/default.nix
+++ modules/nixos/de/wm-addons/plymouth/default.nix
@@ -14,7 +14,25 @@ in {
     boot.initrd.systemd.enable = true;
 
     # hide boot messages
-    boot.kernelParams = [ "quiet" "udev.log_level=3" ];
+    boot.kernelParams = [
+      # silent boot
+      "quiet"
+
+      # stop systemd from printing its version number
+      "udev.log_level=3"
+
+      # suppress successful messages in initramfs
+      "systemd.show_status=auto"
+
+      # stop systemd from printing its version number (initramfs)
+      "rd.udev.log_level=3"
+    ];
+
+    # hide not harmful ACPI BIOS errors (AE_ALREADY_EXISTS) before plymouth
+    # additionally, you can't toggle with esc the logs
+    # additionally, it introduces a minor visual glitch in greetd
+    # more info: https://forum.zorin.com/t/acpi-bios-error-help-to-make-sense-of-this/22035/2
+    # boot.consoleLogLevel = 3;
   };
 }
 
