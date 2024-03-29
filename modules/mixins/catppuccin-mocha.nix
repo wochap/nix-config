c9117d7136df8b3db48070ef57fc63a64ca1cabd
wochap <gean.marroquin@gmail.com>
2024-03-29 09:52:09 -0500

    feat: add catppuccin mocha colors

--- modules/mixins/catppuccin-mocha.nix
+++ modules/mixins/catppuccin-mocha.nix
@@ -1,8 +1,4 @@
 {
-  crust = "#11111b";
-  lavender = "#b4befe";
-  flamingo = "#f2cdcd";
-  mantle = "#181825";
   primary = "#cba6f7";
   background = "#1E1E2E";
   currentline = "#585B70";
@@ -10,11 +6,34 @@
   foreground = "#CDD6F4";
   comment = "#6C7086";
   cyan = "#94E2D5";
-  green = "#A6E3A1";
   orange = "#FAB387";
-  pink = "#F5C2E7";
   purple = "#cba6f7";
-  red = "#F38BA8";
-  yellow = "#F9E2AF";
+
+  rosewater = "#f5e0dc";
+  flamingo = "#f2cdcd";
+  pink = "#f5c2e7";
+  mauve = "#cba6f7";
+  red = "#f38ba8";
+  maroon = "#eba0ac";
+  peach = "#fab387";
+  yellow = "#f9e2af";
+  green = "#a6e3a1";
+  teal = "#94e2d5";
+  sky = "#89dceb";
+  sapphire = "#74c7ec";
+  blue = "#89b4fa";
+  lavender = "#b4befe";
+  text = "#cdd6f4";
+  subtext1 = "#bac2de";
+  subtext0 = "#a6adc8";
+  overlay2 = "#9399b2";
+  overlay1 = "#7f849c";
+  overlay0 = "#6c7086";
+  surface2 = "#585b70";
+  surface1 = "#45475a";
+  surface0 = "#313244";
+  base = "#1e1e2e";
+  mantle = "#181825";
+  crust = "#11111b";
 }
 
