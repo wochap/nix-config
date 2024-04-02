01e65b80e9275e5a234ccface78cbd066379babd
wochap <gean.marroquin@gmail.com>
2024-04-02 13:25:33 -0500

    rice(ags) tray

--- modules/nixos/desktop/wm-addons-wayland/ags/dotfiles/bar/modules/systray.js
+++ modules/nixos/desktop/wm-addons-wayland/ags/dotfiles/bar/modules/systray.js
@@ -6,7 +6,15 @@ export const systray = Widget.Box({
     items.map((item) =>
       Widget.Button({
         child: Widget.Icon({
-          icon: item.bind("icon"),
+          icon: item.bind("icon").as((value) => {
+            if (value === "video-display") {
+              return "kde-windows";
+            }
+            if (value === "smartcode-stremio-tray") {
+              return "smartcode-stremio";
+            }
+            return value;
+          }),
           size: 16,
         }),
         tooltipMarkup: item.bind("tooltip_markup"),
