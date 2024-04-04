b1228cbb419edab94c55dafa97628bce8c1a1c3d
wochap <gean.marroquin@gmail.com>
2024-04-03 19:13:22 -0500

    feat(obs) more plugins

--- modules/nixos/programs/gui/qt/default.nix
+++ modules/nixos/programs/gui/qt/default.nix
@@ -32,12 +32,17 @@ in {
       programs.obs-studio = {
         enable = true;
         plugins = with pkgs; [
+          obs-studio-plugins.wlrobs
+
           obs-studio-plugins.obs-vkcapture
           obs-studio-plugins.obs-gstreamer
           obs-studio-plugins.obs-vaapi
 
           obs-studio-plugins.obs-source-record
-          obs-studio-plugins.obs-backgroundremoval
+          obs-studio-plugins.obs-shaderfilter
+          obs-studio-plugins.obs-gradient-source
+          obs-studio-plugins.obs-rgb-levels-filter
+          # obs-studio-plugins.obs-backgroundremoval # takes too long to build
         ];
       };
 
