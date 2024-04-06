feb35c1e14cad93ddf50229f57a5876021aa5568
wochap <gean.marroquin@gmail.com>
2024-04-06 15:33:11 -0500

    fix typo

--- modules/shared/programs/gui/kitty/scripts/kitty-neomutt.sh
+++ modules/shared/programs/gui/kitty/scripts/kitty-neomutt.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-echo "$@" | xargs -I {} kitty --class kitty-neomutt --title neomutt -e sh -c "neomutt {}"
+set -xe
+
+if [[ $# == 0 ]]; then
+  kitty --class kitty-neomutt --title neomutt -e sh -c neomutt
+else
+  echo "$@" | xargs -I {} kitty --class kitty-neomutt --title neomutt -e sh -c "neomutt {}"
+fi
