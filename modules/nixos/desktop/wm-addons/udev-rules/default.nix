22cf5a4e43105927e4aa9edbf50bd7470387d56f
wochap <gean.marroquin@gmail.com>
2024-05-19 09:42:55 -0500

    feat: disable legion builtin kbd when chocofi is connected

--- modules/nixos/desktop/wm-addons/udev-rules/default.nix
+++ modules/nixos/desktop/wm-addons/udev-rules/default.nix
@@ -7,19 +7,35 @@ let
     name = "99-cornekbd.rules";
     text = ''
       ACTION=="add", SUBSYSTEM=="input", ATTRS{id/vendor}=="4653", ATTRS{id/product}=="0001", ATTRS{name}=="foostan Corne Keyboard", \
-      TAG+="systemd", ENV{SYSTEMD_WANTS}+="disable-glegion-builtin-kbd", ENV{SYSTEMD_ALIAS}="/sys/devices/cornekbd"
+      TAG+="systemd", ENV{SYSTEMD_WANTS}+="cornekbd-disable-glegion-builtin-kbd", ENV{SYSTEMD_ALIAS}="/sys/devices/cornekbd"
     '';
     destination = "/etc/udev/rules.d/99-cornekbd.rules";
+  };
+  chocofikbd = pkgs.writeTextFile {
+    name = "99-chocofikbd.rules";
+    text = ''
+      ACTION=="add", SUBSYSTEM=="input", ATTRS{id/vendor}=="1d50", ATTRS{id/product}=="615e", ATTRS{name}=="Chocochap Keyboard", \
+      TAG+="systemd", ENV{SYSTEMD_WANTS}+="chocofikbd-disable-glegion-builtin-kbd", ENV{SYSTEMD_ALIAS}="/sys/devices/chocofikbd"
+    '';
+    destination = "/etc/udev/rules.d/99-chocofikbd.rules";
   };
 in {
   options._custom.desktop.udev-rules.enable = lib.mkEnableOption { };
 
   config = lib.mkIf cfg.enable {
-    services.udev.packages = [ cornekbd ];
+    services.udev.packages = [ cornekbd chocofikbd ];
 
-    systemd.services.disable-glegion-builtin-kbd = {
+    systemd.services.cornekbd-disable-glegion-builtin-kbd = {
       description = "Disable glegion builtin kbd when Corne kbd is connected";
       bindsTo = [ "sys-devices-cornekbd.device" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.evtest}/bin/evtest --grab /dev/input/event17";
+      };
+    };
+
+    systemd.services.chocofikbd-disable-glegion-builtin-kbd = {
+      description = "Disable glegion builtin kbd when Chocofi kbd is connected";
+      bindsTo = [ "sys-devices-chocofikbd.device" ];
       serviceConfig = {
         ExecStart = "${pkgs.evtest}/bin/evtest --grab /dev/input/event17";
       };
