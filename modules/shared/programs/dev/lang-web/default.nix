f83d4f3aaf8026b71e2ddae6201f91bbe157e3c2
wochap <gean.marroquin@gmail.com>
2024-05-27 14:29:22 -0500

    fix yarn

--- modules/shared/programs/dev/lang-web/default.nix
+++ modules/shared/programs/dev/lang-web/default.nix
@@ -31,6 +31,10 @@ in {
       home.sessionVariables = {
         PATH = "$HOME/.npm-packages/bin:$HOME/.bun/bin:$PATH";
         NODE_PATH = "$HOME/.npm-packages/lib/node_modules:$NODE_PATH";
+
+        # Fixes `bad interpreter: Text file busy`
+        # https://github.com/NixOS/nixpkgs/issues/314713
+        UV_USE_IO_URING = "0";
       };
 
       home.file = {
