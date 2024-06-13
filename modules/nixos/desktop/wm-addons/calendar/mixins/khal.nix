ff668fe99422d1fa55b1f245d9a71f1525c14224
wochap <gean.marroquin@gmail.com>
2024-06-13 12:37:05 -0500

    feat(khal) rice

--- modules/nixos/desktop/wm-addons/calendar/mixins/khal.nix
+++ modules/nixos/desktop/wm-addons/calendar/mixins/khal.nix
@@ -10,29 +10,37 @@ in {
     _custom.hm = {
       home.packages = with pkgs; [ khal ];
 
-      xdg.configFile."khal/config".text = ''
-        [calendars]
+      xdg.configFile."khal/config".text = # toml
+        ''
+          [calendars]
 
-        [[personal_calendar_local]]
-        path = ${dataHome}/vdirsyncer/personal-calendars/*
-        type = discover
+          [[personal_calendar_local]]
+          path = ${dataHome}/vdirsyncer/personal-calendars/*
+          type = discover
 
-        [[boc_calendar_local]]
-        path = ${dataHome}/vdirsyncer/boc-calendars/*
-        type = discover
+          [[boc_calendar_local]]
+          path = ${dataHome}/vdirsyncer/boc-calendars/*
+          type = discover
 
-        [[se_calendar_local]]
-        path = ${dataHome}/vdirsyncer/se-calendars/*
-        type = discover
+          [[se_calendar_local]]
+          path = ${dataHome}/vdirsyncer/se-calendars/*
+          type = discover
 
-        [locale]
-        timeformat = %H:%M
-        dateformat = %Y-%m-%d
-        longdateformat = %Y-%m-%d
-        datetimeformat = %Y-%m-%d %H:%M
-        longdatetimeformat = %Y-%m-%d %H:%M
-        firstweekday = 6
-      '';
+          [locale]
+          timeformat = %H:%M
+          dateformat = %a %d %b
+          longdateformat = %a %d %b %Y
+          datetimeformat = %a %d %b %H:%M
+          longdatetimeformat = %a %d %b %Y %H:%M
+          firstweekday = 0
+
+          [default]
+          highlight_event_days = True
+          # enable_mouse = False
+
+          [view]
+          event_view_always_visible = True
+        '';
     };
   };
 }
