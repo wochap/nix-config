7208b5c5960d66fc09b4cef850d594c0bd095579
wochap <gean.marroquin@gmail.com>
2024-07-06 10:52:07 -0500

    call persistence load after neorg workspace cmd

--- modules/nixos/desktop/wm-addons-wayland/wayland-utils/scripts/tui-notes.sh
+++ modules/nixos/desktop/wm-addons-wayland/wayland-utils/scripts/tui-notes.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kitty --class tui-notes --title neorg -o window_padding_width=0 -e nvim neorg +'cd ~/Sync/neorg' +":lua require('persistence').load()" +'Neorg workspace home'
+kitty --class tui-notes --title neorg -o window_padding_width=0 -e nvim neorg +'cd ~/Sync/neorg' +'Neorg workspace home' +":lua require('persistence').load()"
