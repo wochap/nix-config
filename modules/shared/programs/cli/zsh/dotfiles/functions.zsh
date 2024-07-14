7c23829c3d7d8472802abf4a9bd5fb6bbc5f2250
wochap <gean.marroquin@gmail.com>
2024-07-14 12:12:33 -0500

    feat(zsh): add gsquash fn

--- modules/shared/programs/cli/zsh/dotfiles/functions.zsh
+++ modules/shared/programs/cli/zsh/dotfiles/functions.zsh
@@ -121,3 +121,13 @@ function help() {
   "$@" --help 2>&1 | bathelp
 }
 
+# source: https://stackoverflow.com/questions/5189560/how-do-i-squash-my-last-n-commits-together
+function gsquash() {
+  # Reset the current branch to the commit just before the last 12:
+  git reset --hard "HEAD~$1"
+
+  # HEAD@{1} is where the branch was just before the previous command.
+  # This command sets the state of the index to be as it would just
+  # after a merge from that commit:
+  git merge --squash HEAD@{1}
+}
