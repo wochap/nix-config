5f7c464ef9b1c21cf23dd4d1d32d3564d0e4013b
wochap <gean.marroquin@gmail.com>
2024-07-19 11:27:52 -0500

    ric(tui-bookmarks) remove kitty padding

--- modules/nixos/desktop/wm-addons-wayland/wayland-utils/scripts/tui-bookmarks.sh
+++ modules/nixos/desktop/wm-addons-wayland/wayland-utils/scripts/tui-bookmarks.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo "$@" | xargs -I {} kitty --class tui-bookmarks --title buku -o close_on_child_death=yes -e sh -c "buku-fzf {}"
+echo "$@" | xargs -I {} kitty --class tui-bookmarks --title buku -o window_padding_width=0 -o close_on_child_death=yes -e sh -c "buku-fzf {}"
