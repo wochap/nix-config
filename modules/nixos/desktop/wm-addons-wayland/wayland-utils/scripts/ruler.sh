f01f7d6731af06ecaa560ce3af89a4daa8aa5661
wochap <gean.marroquin@gmail.com>
2024-08-04 08:16:38 -0500

    fix(ruler) freeze screen

--- modules/nixos/desktop/wm-addons-wayland/wayland-utils/scripts/ruler.sh
+++ modules/nixos/desktop/wm-addons-wayland/wayland-utils/scripts/ruler.sh
@@ -5,12 +5,47 @@ EXPIRE_TIME=5000
 # source theme colors
 source "$HOME/.config/scripts/theme-colors.sh"
 
+kill_hyprpicker() {
+  hyprpicker_pid=$(pgrep hyprpicker)
+  if [ -n "$hyprpicker_pid" ]; then
+    kill $hyprpicker_pid
+  fi
+
+}
+
+kill_slurp() {
+  slurp_pid=$(pgrep slurp)
+  if [ -n "$slurp_pid" ]; then
+    kill $slurp_pid
+  fi
+}
+
+freeze_screen() {
+  hyprpicker -r -z &
+  hyprpicker_pid=$!
+  wait $hyprpicker_pid
+
+  # if hyprpicker is killed
+  # kill slurp
+  kill_slurp
+}
+
+if [[ "$1" == "--freeze" ]]; then
+  freeze_screen
+  exit
+fi
+
+sh $0 --freeze &
+sleep 0.1
+
 if [[ -n $(pgrep slurp) ]]; then
   # slurp is already running
   exit 0
 fi
 
 area=$(slurp -d -b "${background}bf" -c "$primary" -F "Iosevka NF" -w 1 -f "%w:%h")
+
+kill_hyprpicker
 
 if [[ -z $area ]]; then
   # slurp canceled
