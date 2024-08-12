687876969e00d57decf7acfcc38c751dd822b7af
wochap <gean.marroquin@gmail.com>
2024-08-12 10:14:39 -0500

    feat(swww) allow passing arg to change topic

--- modules/nixos/desktop/wm-addons-wayland/swww/scripts/swww-unsplash.sh
+++ modules/nixos/desktop/wm-addons-wayland/swww/scripts/swww-unsplash.sh
@@ -9,11 +9,32 @@ topicIdByType=(
   ["travel"]="Fzo3zuOHN6w"
 )
 
+TOPIC="wallpapers"
+
+usage() {
+  echo "Usage: $0 [--topic <topic_type>]"
+  echo "  --topic <topic_type>  Specify the topic type (default: wallpapers)"
+  exit 1
+}
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+  --topic)
+    TOPIC="$2"
+    shift 2
+    ;;
+  *)
+    usage
+    ;;
+  esac
+done
+
 # UNSPLASH_API_KEY=${UNSPLASH_API_KEY:-your_unsplash_api_key}
 UNSPLASH_API_KEY=$(cat ~/nix-config/secrets/unsplash/gean.marroquin@gmail.com/access_key)
 download_path=$(mktemp -d)
 width="3840" # value between 1920 and 3840, snap up to nearest 240 for improved caching
-topic_id=${topicIdByType["wallpapers"]}
+topic_id=${topicIdByType[$TOPIC]}
 
 # Fetch a random image URL from the Unsplash API
 response=$(curl -s -H "Authorization: Client-ID ${UNSPLASH_API_KEY}" \
