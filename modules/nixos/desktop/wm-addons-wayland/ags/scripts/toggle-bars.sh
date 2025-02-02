21b6bd3be291931d7c91654a91853cda0edf1ace
wochap <gean.marroquin@gmail.com>
2025-02-02 16:15:29 -0500

    fix(ags) toggle-bars script

--- modules/nixos/desktop/wm-addons-wayland/ags/scripts/toggle-bars.sh
+++ modules/nixos/desktop/wm-addons-wayland/ags/scripts/toggle-bars.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 wlr-randr --json | jq -c '.[]' | while read -r display; do
-  monitorPlugName=$(echo "$display" | jq -r '.name')
-  is_focused=$(dwl-state "$monitorPlugName" selmon --once | jq -r '.text')
+  monitor_plug_name=$(echo "$display" | jq -r '.name')
+  is_focused=$(dwl-state "$monitor_plug_name" selmon --once | jq -r '.text')
   if [[ "$is_focused" == "1" ]]; then
-    ags --toggle-window "bar-$monitorPlugName"
+    ags --bus-name "$XDG_SESSION_DESKTOP" --toggle-window "bar-$monitor_plug_name"
   fi
 done
