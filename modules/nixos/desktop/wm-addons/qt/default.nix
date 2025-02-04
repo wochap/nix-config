8ab2856312d9da8dd3d22e79937dc425a59e0827
wochap <gean.marroquin@gmail.com>
2025-02-04 11:50:12 -0500

    test qt 6

--- modules/nixos/desktop/wm-addons/qt/default.nix
+++ modules/nixos/desktop/wm-addons/qt/default.nix
@@ -8,6 +8,46 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
+    # nixpkgs.overlays = [
+    #   (final: prev: {
+    #     # source: https://github.com/spikespaz/dotfiles/blob/352ce1a9b728f097bc3062fce95e07d11512a8b9/overlays/patches.nix#L38
+    #     qt6Packages = prev.qt6Packages // {
+    #       qt6ct = prev.qt6Packages.qt6ct.overrideAttrs (self: super: {
+    #         patches = super.patches or [ ] ++ [
+    #           (pkgs.fetchpatch {
+    #             url =
+    #               "https://patch-diff.githubusercontent.com/raw/trialuser02/qt6ct/pull/43.diff";
+    #             hash = "sha256-bCXTbnMia9XzHPVe0dQSwSL7ijElS00wdNkviSrgk1A=";
+    #           })
+    #           (pkgs.fetchpatch {
+    #             url =
+    #               "https://patch-diff.githubusercontent.com/raw/trialuser02/qt6ct/pull/44.diff";
+    #             hash = "sha256-fafLjzPFaIBwMJuFUWISZepPypr6P3SHm6+vIuEdTIY=";
+    #           })
+    #         ];
+    #         buildInputs = super.buildInputs or [ ] ++ (with pkgs.kdePackages; [
+    #           qtdeclarative
+    #           kconfig
+    #           kcolorscheme
+    #           kiconthemes
+    #         ]);
+    #         # Original inputs removed, switch to cmake.
+    #         nativeBuildInputs = with pkgs;
+    #           with pkgs.kdePackages; [
+    #             cmake
+    #             qttools
+    #             wrapQtAppsHook
+    #           ];
+    #         cmakeFlags = [
+    #           "-DPLUGINDIR=${
+    #             placeholder "out"
+    #           }/${pkgs.kdePackages.qtbase.qtPluginPrefix}"
+    #         ];
+    #       });
+    #     };
+    #   })
+    # ];
+
     fonts.packages = with pkgs; [ source-sans-pro ];
 
     environment.systemPackages = with pkgs; [
