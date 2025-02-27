9b71173ff1cfb6df37c0c1f1b0f8ad7746073d01
wochap <gean.marroquin@gmail.com>
2025-02-27 09:26:26 -0500

    fix(tofi-launcher) don't call uwsm-app when tofi-drun returns nothing

--- modules/nixos/desktop/wm-addons-wayland/tofi/scripts/tofi-launcher.sh
+++ modules/nixos/desktop/wm-addons-wayland/tofi/scripts/tofi-launcher.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-uwsm-app -- $(tofi-drun --config "$HOME/.config/tofi/one-line" --drun-launch=false)
+result=$(tofi-drun --config "$HOME/.config/tofi/one-line" --drun-launch=false)
+
+if [[ -z "$result" ]]; then
+  exit 0
+fi
+
+uwsm-app -- $result
