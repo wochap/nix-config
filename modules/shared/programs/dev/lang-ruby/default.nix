49d7c3dfadf72e86671edce7ed6bfaf59c84b2d1
wochap <gean.marroquin@gmail.com>
2025-03-08 15:55:56 -0500

    feat(ruby) add global Gemfile

--- modules/shared/programs/dev/lang-ruby/default.nix
+++ modules/shared/programs/dev/lang-ruby/default.nix
@@ -1,14 +1,25 @@
 { config, pkgs, lib, ... }:
 
-let cfg = config._custom.programs.lang-ruby;
+let
+  cfg = config._custom.programs.lang-ruby;
+  inherit (config._custom.globals) configDirectory;
 in {
   options._custom.programs.lang-ruby.enable = lib.mkEnableOption { };
 
   config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ ruby ];
 
-    environment.systemPackages = with pkgs; [
-      ruby
-    ];
+    _custom.hm = {
+      home = {
+        # Gemfile for LSP, formatters, linters, etc
+        sessionVariables.GLOBAL_GEMFILE = "$HOME/.gem/global/Gemfile";
+
+        file = {
+          ".gem/global/Gemfile".source =
+            lib._custom.relativeSymlink configDirectory ./dotfiles/Gemfile;
+        };
+      };
+    };
   };
 }
 
