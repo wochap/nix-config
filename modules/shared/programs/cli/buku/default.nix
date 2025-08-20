935c0d4b8072ffff34f2083c4d24afeedcd6c032
wochap <gean.marroquin@gmail.com>
2025-08-19 19:43:32 -0500

    fix(buku): add overlay to pull latest revision

--- modules/shared/programs/cli/buku/default.nix
+++ modules/shared/programs/cli/buku/default.nix
@@ -10,6 +10,19 @@ in {
   options._custom.programs.buku.enable = lib.mkEnableOption { };
 
   config = lib.mkIf cfg.enable {
+    nixpkgs.overlays = [
+      (final: prev: {
+        buku = prev.buku.overrideAttrs (oldAttrs: {
+          src = prev.fetchFromGitHub {
+            owner = "jarun";
+            repo = "buku";
+            rev = "641024baa68fa853bf53e593765ca9701a1e3ccb";
+            sha256 = "sha256-PODFG9zBiyLOie1v0Anypxso6jMqVE2L/Vm/oQ/AEXI=";
+          };
+        });
+      })
+    ];
+
     _custom.hm = {
       home = {
         packages = with pkgs; [ buku buku-fzf sqlite ];
