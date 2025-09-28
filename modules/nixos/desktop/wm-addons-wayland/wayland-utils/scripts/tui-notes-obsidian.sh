0cb2fc2d80cbc88fd3d4a1fc2831e22d9b366c44
wochap <gean.marroquin@gmail.com>
2025-09-28 11:52:52 -0500

    fix: empty buffer when opening notes

--- modules/nixos/desktop/wm-addons-wayland/wayland-utils/scripts/tui-notes-obsidian.sh
+++ modules/nixos/desktop/wm-addons-wayland/wayland-utils/scripts/tui-notes-obsidian.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kitty --class tui-notes-obsidian --title obsidian -o window_padding_width=0 -e nvim obsidian +'cd ~/Sync/obsidian/utp'
+IN_OBSIDIAN=true kitty --class tui-notes-obsidian --title obsidian -o window_padding_width=0 -e nvim +'cd ~/Sync/obsidian/utp'
--- modules/nixos/desktop/wm-addons-wayland/wayland-utils/scripts/tui-notes.sh
+++ modules/nixos/desktop/wm-addons-wayland/wayland-utils/scripts/tui-notes.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kitty --class tui-notes --title zk -o window_padding_width=0 -e nvim zk +'cd ~/Sync/zk' +":lua require('persistence').load()"
+IN_ZK=true kitty --class tui-notes --title zk -o window_padding_width=0 -e nvim +'cd ~/Sync/zk' +":lua require('persistence').load()"
