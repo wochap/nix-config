e3046e718318c6717537c3491c136144b914c911
wochap <gean.marroquin@gmail.com>
2025-10-12 09:14:41 -0500

    feat: add cht-sh

--- modules/shared/programs/cli/cht/default.nix
+++ modules/shared/programs/cli/cht/default.nix
@@ -8,7 +8,7 @@ in {
 
   config = lib.mkIf cfg.enable {
     _custom.hm = {
-      home.packages = [ cht ];
+      home.packages = with pkgs; [ cht-sh cht ];
       xdg.configFile."cht/cht-languages.txt".source =
         ./dotfiles/cht-languages.txt;
       xdg.configFile."cht/cht-commands.txt".source =
