e821b9ad826ff2e6889e7dcd7829a3f63b03242c
wochap <gean.marroquin@gmail.com>
2026-07-08 13:53:38 -0500

    feat(gasus) disable nvidia

--- hosts/gasus/hardware.nix
+++ hosts/gasus/hardware.nix
@@ -9,5 +9,15 @@
     # enable wol 54:A0:50:04:B1:69
     interfaces.enp2s0f1.wakeOnLan.enable = true;
   };
+
+  # disable nvidia
+  boot.blacklistedKernelModules = [
+    "nouveau"
+    "nvidia"
+    "nvidiafb"
+    "nvidia-drm"
+    "nvidia-uvm"
+    "nvidia-modeset"
+  ];
 }
 
