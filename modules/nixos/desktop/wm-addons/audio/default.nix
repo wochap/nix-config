8d997ab724d4cc8d830e62171e6e20c5c45d1254
wochap <gean.marroquin@gmail.com>
2026-07-13 21:46:59 -0500

    fix(audio) cracking sound

--- modules/nixos/desktop/wm-addons/audio/default.nix
+++ modules/nixos/desktop/wm-addons/audio/default.nix
@@ -58,7 +58,7 @@ in
           "default.clock.rate" = 48000;
           "default.clock.quantum" = 512;
           "default.clock.min-quantum" = 512;
-          "default.clock.max-quantum" = 8192;
+          "default.clock.max-quantum" = 512;
         };
       };
     };
