1b706d53d434024b9279770283d1655c881e1dee
wochap <gean.marroquin@gmail.com>
2026-08-03 12:36:39 -0500

    fix: mailnotify hashes

--- packages/mailnotify/default.nix
+++ packages/mailnotify/default.nix
@@ -8,20 +8,19 @@
 
 buildGoModule rec {
   pname = "mailnotify";
-  version = "0.1.0-next";
+  version = "206290eadf323ce60f6abfd0a6f968af60c0ccca";
+  vendorHash = "sha256-8COiuVhQQ8aBDT5/q8Pt6ZTGbB3+xKDlKVA3BsFkF/A=";
 
   src = fetchFromGitHub {
     owner = "wochap";
     repo = pname;
-    rev = "d771510278c63052b7c591e75f451ad38708a970";
-    sha256 = "sha256-LDZFQ2YRNQW96MAKsw4H1/uqXrNWDEP0AbBChD9HxJI=";
+    rev = version;
+    sha256 = "sha256-3kn6tq3RIS0dYP1XqcuLu2uQWgqb461q759oFfp5HcI=";
   };
-
-  vendorHash = "sha256-Mj1vte+bnDmY/tn6+GXX9IwIKgy9J4QvoIP/pLcID6E=";
 
   meta = with lib; {
     description = "A small program that notifies when mail has arrived in your mail directory.";
-    homepage = "https://github.com/sumnerevans/mailnotify";
+    homepage = "https://github.com/wochap/mailnotify";
     license = licenses.gpl3Plus;
   };
 }
