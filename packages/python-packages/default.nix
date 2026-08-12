09588b72f55f5ddc99ad8fd196a0ee35fabf5771
wochap <gean.marroquin@gmail.com>
2026-08-12 11:14:35 -0500

    remove vidcutter

--- packages/python-packages/default.nix
+++ packages/python-packages/default.nix
@@ -13,9 +13,6 @@
   python-remind = pkgs.prevstable-python.callPackage ./python-remind.nix {
     pkgs = pkgs.prevstable-python;
   };
-  vidcutter = pkgs.prevstable-python.callPackage ./vidcutter.nix {
-    pkgs = pkgs.prevstable-python;
-  };
   pix2tex = pkgs.prevstable-python.callPackage ./pix2tex {
     pkgs = pkgs.prevstable-python;
   };
