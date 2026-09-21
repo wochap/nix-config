f672cd6a15b6f382ff0d20cba0ffffe2efdada5c
wochap <gean.marroquin@gmail.com>
2026-09-21 12:33:59 -0500

    feat(syncthing) make it available in syncthing.wochap.local

--- modules/nixos/services/syncthing/default.nix
+++ modules/nixos/services/syncthing/default.nix
@@ -1,15 +1,39 @@
-{ config, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 let
   cfg = config._custom.services.syncthing;
+  inherit (pkgs._custom) wochap-ssc;
+  proxy = config._custom.services.web-proxies.syncthing;
 in
 {
   options._custom.services.syncthing.enable = lib.mkEnableOption { };
 
   config = lib.mkIf cfg.enable {
+    # https://syncthing.wochap.local
+    _custom.services.web-proxies.syncthing = {
+      enable = true;
+      subdomain = "syncthing";
+      publicPort = 8383;
+      backendPort = 8384;
+      # keep syncthing always running, nginx hits backendPort directly
+      lazy = false;
+    };
+
     _custom.hm = {
       services.syncthing = {
         enable = true;
+        guiAddress = "${wochap-ssc.meta.address}:${toString proxy.backendPort}";
+        # nginx forwards Host as syncthing.wochap.local, syncthing rejects
+        # non-localhost hosts unless this is set
+        settings.gui.insecureSkipHostcheck = true;
+        # settings != {} triggers syncthing-init, keep folders/devices added via GUI
+        overrideDevices = false;
+        overrideFolders = false;
         # openDefaultPorts = true;
         # relay.enable = false;
         # settings.options = {
